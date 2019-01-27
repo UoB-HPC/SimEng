4dcf62daff9a8c425467bf62a95749eec7ad5270
Andrei Poenaru <ap13004@my.bristol.ac.uk>
2019-01-27 17:09:33 +0000

    :Add unit testing using googletest.

---
+++ test/ISATest.cc
@@ -0,0 +1,26 @@
+#include "RegisterFile.hh"
+#include "A64Architecture.hh"
+#include "A64Instruction.hh"
+#include "gtest/gtest.h"
+
+namespace {
+
+// Test that we can create an A64Architecture object
+TEST(ISATest, CreateA64Arch) {
+  std::unique_ptr<simeng::Architecture> isa =
+      std::make_unique<simeng::A64Architecture>();
+
+  EXPECT_GT(isa->getRegisterFileStructure().size(), 0);
+}
+
+// Test that we can set a value in a register file
+TEST(ISATest, CreateRegisterFile) {
+    auto registerFile = simeng::RegisterFile({32, 32, 1});
+    auto reg          = simeng::Register{ simeng::A64RegisterType::GENERAL, 0 };
+
+    registerFile.set(reg, 42);
+    
+    EXPECT_TRUE(registerFile.get(reg));
+}
+
+} // namespace 
