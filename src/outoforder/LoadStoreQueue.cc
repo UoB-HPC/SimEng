cf163c56b107b856f5e4b0cf976072d215f941b3
Hal Jones <hal.jones@bristol.ac.uk>
2019-02-12 16:50:19 +0000

    Adds an initial load/store queue model

---
+++ src/outoforder/LoadStoreQueue.cc
@@ -0,0 +1,123 @@
+#include "LoadStoreQueue.hh"
+
+#include <cassert>
+#include <cstring>
+
+namespace simeng {
+namespace outoforder {
+
+LoadStoreQueue::LoadStoreQueue(unsigned int maxCombinedSpace, char* memory)
+    : maxCombinedSpace(maxCombinedSpace), combined(true), memory(memory){};
+
+LoadStoreQueue::LoadStoreQueue(unsigned int maxLoadQueueSpace,
+                               unsigned int maxStoreQueueSpace, char* memory)
+    : maxLoadQueueSpace(maxLoadQueueSpace),
+      maxStoreQueueSpace(maxStoreQueueSpace),
+      combined(false),
+      memory(memory){};
+
+unsigned int LoadStoreQueue::getLoadQueueSpace() const {
+  if (combined) {
+    return getCombinedSpace();
+  } else {
+    return getLoadQueueSplitSpace();
+  }
+}
+unsigned int LoadStoreQueue::getStoreQueueSpace() const {
+  if (combined) {
+    return getCombinedSpace();
+  } else {
+    return getStoreQueueSplitSpace();
+  }
+}
+unsigned int LoadStoreQueue::getTotalSpace() const {
+  if (combined) {
+    return getCombinedSpace();
+  } else {
+    return getLoadQueueSplitSpace() + getStoreQueueSplitSpace();
+  }
+}
+
+unsigned int LoadStoreQueue::getLoadQueueSplitSpace() const {
+  return maxLoadQueueSpace - loadQueue.size();
+}
+unsigned int LoadStoreQueue::getStoreQueueSplitSpace() const {
+  return maxStoreQueueSpace - storeQueue.size();
+}
+unsigned int LoadStoreQueue::getCombinedSpace() const {
+  return maxCombinedSpace - loadQueue.size() - storeQueue.size();
+}
+
+void LoadStoreQueue::addLoad(const std::shared_ptr<Instruction>& insn) {
+  loadQueue.push_back(insn);
+}
+void LoadStoreQueue::addStore(const std::shared_ptr<Instruction>& insn) {
+  storeQueue.push_back(insn);
+}
+
+void LoadStoreQueue::startLoad(const std::shared_ptr<Instruction>& insn) {
+  // TODO: Defer data read
+  const auto& addresses = insn->getGeneratedAddresses();
+  for (auto const& request : addresses) {
+    // Pointer manipulation to generate a RegisterValue from an arbitrary
+    // memory address
+    auto buffer = malloc(request.second);
+    memcpy(buffer, memory + request.first, request.second);
+
+    auto ptr = std::shared_ptr<uint8_t>((uint8_t*)buffer, free);
+    auto data = RegisterValue(ptr);
+
+    insn->supplyData(request.first, data);
+  }
+}
+
+void LoadStoreQueue::commitStore() {
+  assert(storeQueue.size() > 0 &&
+         "Attempted to commit a store from an empty queue");
+
+  const auto& uop = storeQueue.front();
+  auto addresses = uop->getGeneratedAddresses();
+  auto data = uop->getData();
+  for (size_t i = 0; i < addresses.size(); i++) {
+    auto request = addresses[i];
+
+    // Copy data to memory
+    auto address = memory + request.first;
+    memcpy(address, data[i].getAsVector<void>(), request.second);
+  }
+
+  // TODO: Search load queue for memory order violations and flush if discovered
+
+  storeQueue.pop_front();
+}
+
+void LoadStoreQueue::commitLoad() {
+  assert(loadQueue.size() > 0 &&
+         "Attempted to commit a load from an empty queue");
+
+  loadQueue.pop_front();
+}
+
+void LoadStoreQueue::purgeFlushed() {
+  auto it = loadQueue.begin();
+  while (it != loadQueue.end()) {
+    auto& entry = *it;
+    if (entry->isFlushed()) {
+      it = loadQueue.erase(it);
+    } else {
+      it++;
+    }
+  }
+
+  while (it != storeQueue.end()) {
+    auto& entry = *it;
+    if (entry->isFlushed()) {
+      it = storeQueue.erase(it);
+    } else {
+      it++;
+    }
+  }
+}
+
+}  // namespace outoforder
+}  // namespace simeng
