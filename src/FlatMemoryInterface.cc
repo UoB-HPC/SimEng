d1f3ce50baf4e92f43d5706c6f3f60ec850c0f7f
Hal Jones <hal.jones@bristol.ac.uk>
2019-05-09 13:15:53 +0100

    Adds a flat memory interface implementation

---
+++ src/FlatMemoryInterface.cc
@@ -0,0 +1,39 @@
+#include "FlatMemoryInterface.hh"
+
+#include <cassert>
+
+namespace simeng {
+
+FlatMemoryInterface::FlatMemoryInterface(char* memory, size_t size)
+    : memory_(memory), size_(size) {}
+
+void FlatMemoryInterface::requestRead(const MemoryAccessTarget& target) {
+  assert(target.address + target.size <= size_ &&
+         "Attempted to read beyond memory limit");
+
+  const char* ptr = memory_ + target.address;
+
+  // Copy the data at the requested memory address into a RegisterValue
+  completedReads_.push_back({target, RegisterValue(ptr, target.size)});
+}
+
+void FlatMemoryInterface::requestWrite(const MemoryAccessTarget& target,
+                                       const RegisterValue& data) {
+  assert(target.address + target.size <= size_ &&
+         "Attempted to write beyond memory limit");
+
+  auto ptr = memory_ + target.address;
+  // Copy the data from the RegisterValue to memory
+  memcpy(ptr, data.getAsVector<char>(), target.size);
+}
+
+const span<std::pair<MemoryAccessTarget, RegisterValue>>
+FlatMemoryInterface::getCompletedReads() const {
+  return {const_cast<std::pair<MemoryAccessTarget, RegisterValue>*>(
+              completedReads_.data()),
+          completedReads_.size()};
+}
+
+void FlatMemoryInterface::clearCompletedReads() { completedReads_.clear(); }
+
+}  // namespace simeng
