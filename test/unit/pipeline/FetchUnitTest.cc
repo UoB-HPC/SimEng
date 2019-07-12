1ba195785c03a3b1cb2456b95d53e6cce8b59ec9
James Price <j.price@bristol.ac.uk>
2019-07-12 15:53:37 +0100

    Add test for unaligned instruction fetch

--- test/unit/pipeline/FetchUnitTest.cc
+++ test/unit/pipeline/FetchUnitTest.cc
@@ -21,7 +21,7 @@ class PipelineFetchUnitTest : public testing::Test {
  public:
   PipelineFetchUnitTest()
       : output(1, {}),
-        fetchBuffer({0, 4}, 0),
+        fetchBuffer({0, 16}, 0),
         completedReads(&fetchBuffer, 1),
         fetchUnit(output, memory, 1024, 0, 4, isa, predictor) {}
 
@@ -75,6 +75,31 @@ TEST_F(PipelineFetchUnitTest, TickStalled) {
 
   // Verify that nothing was pushed to the output
   EXPECT_EQ(output.getTailSlots()[0].size(), 0);
+}
+
+// Tests that the fetch unit will handle instructions that straddle fetch block
+// boundaries by automatically requesting the next block of data.
+TEST_F(PipelineFetchUnitTest, FetchUnaligned) {
+  ON_CALL(isa, getMaxInstructionSize()).WillByDefault(Return(4));
+  ON_CALL(memory, getCompletedReads()).WillByDefault(Return(completedReads));
+
+  // Set PC to 14, so there will not be enough data to start decoding
+  EXPECT_CALL(isa, predecode(_, _, _, _, _)).Times(0);
+  fetchUnit.updatePC(14);
+  fetchUnit.tick();
+
+  // Expect a block starting at address 16 to be requested when we fetch again
+  EXPECT_CALL(memory, requestRead(Field(&MemoryAccessTarget::address, 16)))
+      .Times(1);
+  fetchUnit.requestFromPC();
+
+  // Tick again, expecting that decoding will now resume
+  std::pair<MemoryAccessTarget, RegisterValue> nextBlockValue = {{16, 16}, 0};
+  span<std::pair<MemoryAccessTarget, RegisterValue>> nextBlock = {
+      &nextBlockValue, 1};
+  EXPECT_CALL(memory, getCompletedReads()).WillOnce(Return(nextBlock));
+  EXPECT_CALL(isa, predecode(_, _, _, _, _)).WillOnce(Return(4));
+  fetchUnit.tick();
 }
 
 }  // namespace pipeline
