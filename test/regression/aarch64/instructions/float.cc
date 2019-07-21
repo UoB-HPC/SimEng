b77a3dca086d916bc0247139da6fe936dd4c92b5
James Price <j.price@bristol.ac.uk>
2019-07-21 14:49:33 +0100

    [AArch64] Implement 64-bit fmov from immediate
    Fix the vector version as well.

---
+++ test/regression/aarch64/instructions/float.cc
@@ -0,0 +1,30 @@
+#include "AArch64RegressionTest.hh"
+
+namespace {
+
+using InstFloat = AArch64RegressionTest;
+
+TEST_P(InstFloat, fmov) {
+  RUN_AARCH64(R"(
+    fmov d0, 1.0
+    fmov d1, -0.125
+  )");
+  EXPECT_EQ((getVectorRegisterElement<double, 0>(0)), 1.0);
+  EXPECT_EQ((getVectorRegisterElement<double, 1>(0)), 0.0);
+  EXPECT_EQ((getVectorRegisterElement<double, 0>(1)), -0.125);
+  EXPECT_EQ((getVectorRegisterElement<double, 1>(1)), 0.0);
+
+  RUN_AARCH64(R"(
+    fmov v0.2d, 1.0
+    fmov v1.2d, -0.125
+  )");
+  EXPECT_EQ((getVectorRegisterElement<double, 0>(0)), 1.0);
+  EXPECT_EQ((getVectorRegisterElement<double, 1>(0)), 1.0);
+  EXPECT_EQ((getVectorRegisterElement<double, 0>(1)), -0.125);
+  EXPECT_EQ((getVectorRegisterElement<double, 1>(1)), -0.125);
+}
+
+INSTANTIATE_TEST_SUITE_P(AArch64, InstFloat, ::testing::Values(EMULATION),
+                         coreTypeToString);
+
+}  // namespace
