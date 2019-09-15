b33636d9dd46478b7e6dd94e36d370f9e908ebc8
Simon McIntosh-Smith <simonm@cs.bris.ac.uk>
2019-09-15 19:10:49 +0100

    Added branch counters to the inorder model
    Now matches up with the outoforder model.

--- src/lib/models/inorder/Core.cc
+++ src/lib/models/inorder/Core.cc
@@ -134,10 +134,21 @@ uint64_t Core::getSystemTimer() const {
 std::map<std::string, std::string> Core::getStats() const {
   auto retired = writebackUnit_.getInstructionsWrittenCount();
   auto ipc = retired / static_cast<float>(ticks_);
+
+  // Sum up the branch stats reported across the execution units.
+  uint64_t totalBranchesExecuted = 0;
+  uint64_t totalBranchMispredicts = 0;
+  totalBranchesExecuted += executeUnit_.getBranchExecutedCount();
+  totalBranchMispredicts += executeUnit_.getBranchMispredictedCount();
+  auto branchMissRate = 100.0f * static_cast<float>(totalBranchMispredicts) / static_cast<float>(totalBranchesExecuted);
+
   return {{"cycles", std::to_string(ticks_)},
           {"retired", std::to_string(retired)},
           {"ipc", std::to_string(ipc)},
-          {"flushes", std::to_string(flushes_)}};
+          {"flushes", std::to_string(flushes_)},
+          {"branch.executed", std::to_string(totalBranchesExecuted)},
+          {"branch.mispredict", std::to_string(totalBranchMispredicts)},
+          {"branch.missrate", std::to_string(branchMissRate)}};
 }
 
 void Core::raiseException(const std::shared_ptr<Instruction>& instruction) {
