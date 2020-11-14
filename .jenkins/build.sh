d435093f8e1e62f464ad5265bddef88fb6b33aef
Harry Waugh <harry.waugh@bristol.ac.uk>
2020-11-14 13:51:49 +0000

    Cleanup handled manually to save time

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 
-## Set up file structure
-mkdir -p build install
-export SIMENG_TOP=$PWD
-export SIMENG_BUILD=$PWD/build
-export SIMENG_INSTALL=$PWD/install
 
 
 build_and_run () {
+   rm -rf build/* install/*
+
    cd $SIMENG_BUILD
    cmake $SIMENG_TOP -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SIMENG_INSTALL
    make -j
@@ -16,11 +13,16 @@ build_and_run () {
    make tests
 }
 
+## Set up file structure
+export SIMENG_TOP=$PWD
+export SIMENG_BUILD=$PWD/build
+export SIMENG_INSTALL=$PWD/install
 
-if [ ! -d "/home/br-hwaugh/jenkins/workspace/SimEngBuild" ]
+if [ ! -d "$SIMENG_TOP" ]
 then
     cd /home/br-hwaugh/jenkins/workspace/
     git clone --recurse-submodules https://github.com/UoB-HPC/SimEng.git
+    cd SimEngBuild
     ## Download and update submodules
     git submodule update --init
     git submodule sync --recursive
@@ -28,6 +30,15 @@ then
     cd external/capstone
     git fetch
     git checkout next
+
+    cd $SIMENG_TOP
+    mkdir -p build install
+else
+    cd $SIMENG_TOP
+    rm -rf build/* install/*
+
+    git stash
+    git checkout ${ghprbActualCommit}
 fi
 
 
