e45dcb8c5acef76459719f3fc4f1226d2ed4dd03
Harry Waugh <harry.waugh@bristol.ac.uk>
2020-11-14 14:15:42 +0000

    Added armclang

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -49,20 +49,24 @@ fi
 export CMAKE_C_COMPILER=cc
 export CMAKE_CXX_COMPILER=CC
 
+echo "Compiler Armclang 20.0"
+module swap PrgEnv-cray PrgEnv-allinea
+module swap Generic-AArch64/SUSE/12/arm-linux-compiler/20.0 ThunderX2CN99/SUSE/12/arm-linux-compiler-20.0/armpl/20.0.0
+module load tools/cmake
+build_and_run
+
 echo "Compiler GCC 7"
-module swap PrgEnv-cray PrgEnv-gnu
+module swap PrgEnv-allinea PrgEnv-gnu
 module swap gcc gcc/7.3.0
 module load tools/cmake
 build_and_run
 
 echo "Compiler GCC 8"
-module swap PrgEnv-cray PrgEnv-gnu
 module swap gcc gcc/8.3.0
 module load tools/cmake
 build_and_run
 
 echo "Compiler GCC 9"
-module swap PrgEnv-cray PrgEnv-gnu
 module swap gcc gcc/9.3.0
 module load tools/cmake
 build_and_run
