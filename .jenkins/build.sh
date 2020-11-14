3b8598771f10779a3e81afa2367b08b2fcd32517
Harry Waugh <harry.waugh@bristol.ac.uk>
2020-11-14 10:03:11 +0000

    Updated build for multiple GCC compilers

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
-module swap PrgEnv-cray PrgEnv-gnu
-module load tools/cmake/3.15.4
-
-echo "JENKINS BUILD SCRIPT"
-echo "MODULES"
-module li
-echo "CURRENT DIRECTORY""
-pwd
-echo "GIT BRANCH
-git branch
 
 
-mkdir -p SimEngBuild/install
-cd SimEngBuild
+## Set up file structure
+mkdir -p build install
 export SIMENG_TOP=$PWD
+export SIMENG_BUILD=$PWD/build
+export SIMENG_INSTALL=$PWD/install
 
 
-git clone --recurse-submodules https://github.com/UoB-HPC/SimEng.git
-cd SimEng
-git pull origin master
+build_and_run () {
+   echo "Compiler version"
+   cc -v
+
+   cd $SIMENG_BUILD
+   cmake $SIMENG_TOP -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SIMENG_INSTALL
+   make
+   make install
+
+   make tests
+}
+
+
+## Download and update submodules
 git submodule update --init
-git checkout review
 git submodule sync --recursive
 
 cd external/capstone
 git fetch
 git checkout next
 
-cd $SIMENG_TOP
+## Setup environment
 export CMAKE_C_COMPILER=cc
 export CMAKE_CXX_COMPILER=CC
-cmake $SIMENG_TOP/SimEng -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SIMENG_TOP/install
-make
-make install
 
-make tests
+## Load GCC 7
+module swap PrgEnv-cray PrgEnv-gnu
+module swap gcc gcc/7.3.0
+module load tools/cmake
+build_and_run
+
+## Load GCC 8
+module swap PrgEnv-cray PrgEnv-gnu
+module swap gcc gcc/8.3.0
+module load tools/cmake
+build_and_run
+
+## Load GCC 9
+module swap PrgEnv-cray PrgEnv-gnu
+module swap gcc gcc/9.3.0
+module load tools/cmake
+build_and_run
+
