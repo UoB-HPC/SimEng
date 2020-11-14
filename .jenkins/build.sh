7e829d62e7e9abac78fbc53c60505415c072cd3f
Harry Waugh <harry.waugh@bristol.ac.uk>
2020-11-14 10:43:34 +0000

    Removed compiler version as it halts build

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -9,9 +9,6 @@ export SIMENG_INSTALL=$PWD/install
 
 
 build_and_run () {
-   echo "Compiler version"
-   cc -v
-
    cd $SIMENG_BUILD
    cmake $SIMENG_TOP -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SIMENG_INSTALL
    make
@@ -33,19 +30,19 @@ git checkout next
 export CMAKE_C_COMPILER=cc
 export CMAKE_CXX_COMPILER=CC
 
-## Load GCC 7
+echo "Compiler GCC 7"
 module swap PrgEnv-cray PrgEnv-gnu
 module swap gcc gcc/7.3.0
 module load tools/cmake
 build_and_run
 
-## Load GCC 8
+echo "Compiler GCC 8"
 module swap PrgEnv-cray PrgEnv-gnu
 module swap gcc gcc/8.3.0
 module load tools/cmake
 build_and_run
 
-## Load GCC 9
+echo "Compiler GCC 9"
 module swap PrgEnv-cray PrgEnv-gnu
 module swap gcc gcc/9.3.0
 module load tools/cmake
