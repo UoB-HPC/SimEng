1e2e350ad417f7f2c51afd705f9f7b1843067c2e
Harry Waugh <harry.waugh@bristol.ac.uk>
2020-12-04 12:24:15 +0000

    Adding intel compiler paths manually. Jenkins MACS not liking modules

--- .jenkins/build_intel19.sh
+++ .jenkins/build_intel19.sh
@@ -6,11 +6,18 @@ source .jenkins/build_and_run.sh
 checkout
 
 ## Setup environment
+
+PATH=/cm/shared/apps/intel_parallel_studio_xe_2019_update4/compilers_and_libraries_2019.4.243/linux/bin/intel64/:$PATH
+
 export CMAKE_C_COMPILER=icc
+export CC=icc
 export CMAKE_CXX_COMPILER=icpc
+export CXX=icpc
+
+
 
 echo "Compiler INTEL 19"
-module load intel-parallel-studio-xe/compilers/64/2019u4/19.0.4 intel-parallel-studio-xe/mpi/64/2019u4/4.243
+#module load intel-parallel-studio-xe/compilers/64/2019u4/19.0.4 intel-parallel-studio-xe/mpi/64/2019u4/4.243
 export PATH=/home/br-hwaugh/installations/cmake-3.18.5/bin/:$PATH
 build
 
