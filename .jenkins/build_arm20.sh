89c13e9fbac5267a32348b0218b7ca70106386ee
jj16791 <jj16791@bristol.ac.uk>
2021-06-21 11:33:03 +0100

    gcc7 build update and addition of SimEng default program runs

--- .jenkins/build_arm20.sh
+++ .jenkins/build_arm20.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-source .jenkins/build_and_run.sh
-
+source .jenkins/build_test_run.sh
 
 ## Download/clean and checkout pull request
 checkout
@@ -10,12 +9,13 @@ checkout
 export CMAKE_C_COMPILER=cc
 export CMAKE_CXX_COMPILER=CC
 
-
-## Load compilers
+## Load compilers/libraries
 echo "Compiler Armclang 20.0"
 module swap PrgEnv-cray PrgEnv-allinea
 module swap Generic-AArch64/SUSE/12/arm-linux-compiler/20.0 ThunderX2CN99/SUSE/12/arm-linux-compiler-20.0/armpl/20.0.0
 module load tools/cmake
-debug
+
+## Build, test, and run SimEng
 build
+test
 run
--- .jenkins/build_gcc8.sh
+++ .jenkins/build_gcc8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source .jenkins/build_and_run.sh
+source .jenkins/build_test_run.sh
 
 ## Download/clean and checkout pull request
 checkout
@@ -9,9 +9,13 @@ checkout
 export CMAKE_C_COMPILER=cc
 export CMAKE_CXX_COMPILER=CC
 
+## Load compilers/libraries
 echo "Compiler GCC 8"
 module swap PrgEnv-cray PrgEnv-gnu
 module swap gcc gcc/8.3.0
 module load tools/cmake
+
+## Build, test, and run SimEng
 build
+test
 run
