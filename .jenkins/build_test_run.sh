d65d13a862b2b399456191a2b68858228cc78ba7
jj16791 <jj16791@bristol.ac.uk>
2021-06-21 15:20:44 +0100

    Printouts for simulation runs

--- .jenkins/build_test_run.sh
+++ .jenkins/build_test_run.sh
@@ -53,21 +53,74 @@ test () {
 # Run default program with and without specified configuration
 run () {
     cd "$SIMENG_INSTALL" || exit
-    echo "Runnning default program with built in config"
-    ./bin/simeng > run
-    if [[ "$(tail -n 3 run | head -n 1)" != "retired: 3145732" ]]
-    then
-        echo "ERROR: ${STAGE_NAME} run without passed config failed. Output:"
-        cat run
-        false
-    fi
 
-    echo "Runnning default program with passed config"
+    ./bin/simeng > run
+    echo "Simulation without configuration file argument:"
+    cat run
+    # Ensure the correct number of instructions were retired
+    # if [[ "$(tail -n 3 run | head -n 1)" != "retired: 3145732" ]]
+    # then
+    #     echo "ERROR: ${STAGE_NAME} run without passed config failed due to an incorrect number of retired instructions."
+    #     echo -e "\tExpect retired: 3145731"
+    #     echo -e "\tGot $(tail -n 3 run | head -n 1)"
+    #     echo "Full output:"
+    #     cat run
+    #     echo ""
+    #     false
+    # fi
+    echo ""
+    compare_outputs "$(tail -n 3 run | head -n 1)" "retired: 3145732" "retired instructions"
+
+    # if [[ "$(tail -n 1 run | cut -c9-17)" != "3145739" ]]
+    # then
+    #     echo "ERROR: ${STAGE_NAME} run without passed config failed due to an incorrect number of simulated ticks."
+    #     echo -e "\tExpect 3145738"
+    #     echo -e "\tGot $(tail -n 1 run | cut -c10-16)"
+    #     echo "Full output:"
+    #     cat run
+    #     echo ""
+    #     false
+    # fi
+    compare_outputs "$(tail -n 1 run | cut -c10-16)" "3145739" "simulated ticks"
+    echo ""
+
     ./bin/simeng "$SIMENG_TOP"/configs/tx2.yaml > run
-    if [[ "$(tail -n 3 run | head -n 1)" != "retired: 3145731" ]]
+    echo "Simulation with configuration file argument:"
+    cat run
+    # if [[ "$(tail -n 3 run | head -n 1)" != "retired: 3145731" ]]
+    # then
+    #     echo "ERROR: ${STAGE_NAME} run without passed config failed due to an incorrect number of retired instructions."
+    #     echo -e "\tExpect retired: 3145732"
+    #     echo -e "\tGot $(tail -n 3 run | head -n 1)"
+    #     echo "Full output:"
+    #     cat run
+    #     echo ""
+    #     false
+    # fi
+    echo ""
+    compare_outputs "$(tail -n 3 run | head -n 1)" "retired: 3145731" "retired instructions"
+
+    # if [[ "$(tail -n 1 run | cut -c9-17)" != " 1048594 " ]]
+    # then
+    #     echo "ERROR: ${STAGE_NAME} run with passed config failed due to an incorrect number of simulated ticks."
+    #     echo -e "\tExpect 1048593"
+    #     echo -e "\tGot $(tail -n 1 run | cut -c10-16)"
+    #     echo "Full output:"
+    #     cat run
+    #     echo ""
+    #     false
+    # fi
+    compare_outputs "$(tail -n 1 run | cut -c10-16)" "1048594" "simulated ticks"
+    echo ""
+}
+
+# Helper function for checking outputs
+compare_outputs() {
+    if [[ $1 != $2 ]]
     then
-        echo "ERROR: ${STAGE_NAME} run with passed config failed. Output:"
-        cat run
+        echo "ERROR: ${STAGE_NAME} run failed due to an incorrect number of $3."
+        echo -e "\tExpect \"$1\""
+        echo -e "\tGot \"$2\""
         false
     fi
 }
