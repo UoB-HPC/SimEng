0a17f9612ee364083cfdc03237bbd5d8d9495ff4
Mutalib Mohammed <ms17276@bristol.ac.uk>
2021-08-13 16:24:16 +0400

    Fix out-of-bounds memory read logic in FixedLatencyMemoryInterface (#183)
    Within this PR, additional bound checking functionality has been added to the FixedLatencyMemoryInterface tick function to prevent unsigned overflow.

--- test/unit/LatencyMemoryInterfaceTest.cc
+++ test/unit/LatencyMemoryInterfaceTest.cc
@@ -28,4 +28,36 @@ TEST(LatencyMemoryInterfaceTest, FixedWriteData) {
   EXPECT_EQ(memoryData, 0xDEADBEEF);
 }
 
+// Test that out-of-bounds memory reads are correctly handled.
+TEST(LatencyMemoryInterfaceTest, OutofBoundsRead) {
+  uint32_t memoryData = 0;
+  simeng::FixedLatencyMemoryInterface memory(
+      reinterpret_cast<char*>(&memoryData), 4, 1);
+
+  // Create a target such that address + size will overflow
+  simeng::MemoryAccessTarget overflowTarget = {UINT64_MAX, 4};
+  memory.requestRead(overflowTarget, 1);
+
+  // Create a regular out-of-bounds target
+  simeng::MemoryAccessTarget target = {0, 8};
+  memory.requestRead(target, 2);
+
+  // Tick once - request should have completed
+  memory.tick();
+  EXPECT_FALSE(memory.hasPendingRequests());
+
+  auto entries = memory.getCompletedReads();
+  EXPECT_EQ(entries.size(), 2);
+
+  auto overflowResult = entries[0];
+  EXPECT_EQ(overflowResult.requestId, 1);
+  EXPECT_EQ(overflowResult.data, simeng::RegisterValue());
+  EXPECT_EQ(overflowResult.target, overflowTarget);
+
+  auto result = entries[1];
+  EXPECT_EQ(result.requestId, 2);
+  EXPECT_EQ(result.data, simeng::RegisterValue());
+  EXPECT_EQ(result.target, target);
+}
+
 }  // namespace
