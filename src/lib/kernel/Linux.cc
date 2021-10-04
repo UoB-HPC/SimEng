3fbfad98ab2b2d6c84f1f18fe89e2678aea7d095
jj16791 <jj16791@bristol.ac.uk>
2021-10-04 23:47:01 +0100

    Changed enums used in Linux::clockGetTime to their actual values to prevent errors from differing systems' underlying values for said enums

--- src/lib/kernel/Linux.cc
+++ src/lib/kernel/Linux.cc
@@ -59,12 +59,11 @@ uint64_t Linux::clockGetTime(uint64_t clkId, uint64_t systemTimer,
                              uint64_t& seconds, uint64_t& nanoseconds) {
   // TODO: Ideally this should get the system timer from the core directly
   // rather than having it passed as an argument.
-
-  if (clkId == CLOCK_REALTIME) {
+  if (clkId == 0) {  // CLOCK_REALTIME
     seconds = systemTimer / 1e9;
     nanoseconds = systemTimer - (seconds * 1e9);
     return 0;
-  } else if (clkId == CLOCK_MONOTONIC) {
+  } else if (clkId == 1) {  // CLOCK_MONOTONIC
     seconds = systemTimer / 1e9;
     nanoseconds = systemTimer - (seconds * 1e9);
     return 0;
