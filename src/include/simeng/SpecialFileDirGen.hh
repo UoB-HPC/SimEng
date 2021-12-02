23cb1ae4e75b60bf23bc23b8864854982461e4ce
finnwilkinson <finnwilkinson5@gmail.com>
2021-12-02 15:06:41 +0000

    Generated new class to generate special files dir. Also modified the ModelConfig code and Main to support the new fields in the YAML config file, validate the values in the config file, and initiate special files dir generation if indicated.

---
+++ src/include/simeng/SpecialFileDirGen.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace simeng {
+class SpecialFileDirGen {
+ public:
+  /** Construct a SpecialFileDirGen class by reading in the YAML file and
+   * running it through checks and formatting. */
+  SpecialFileDirGen(std::string path);
+
+ private:
+}
+}  // namespace simeng
