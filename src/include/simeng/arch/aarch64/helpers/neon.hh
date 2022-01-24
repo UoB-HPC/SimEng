f2a56c15310d7adecd3c954dba4510a81f6d441f
Finn Wilkinson <finnwilkinson5@gmail.com>
2022-01-24 11:10:42 +0000

    Implemented SVE fsqrt (predicated, 2vecs) helper function.

--- src/include/simeng/arch/aarch64/helpers/neon.hh
+++ src/include/simeng/arch/aarch64/helpers/neon.hh
@@ -358,6 +358,21 @@ class neonHelp {
     return {out, 256};
   }
 
+  /** Helper function for NEON instructions with the format `fsqrt vd, vn`.
+   *I represents the number of elements in the output array to be updated (i.e.
+   *for vd.2s the final 8 elements in the output array will be 0).
+   */
+  template <typename T, int I>
+  static RegisterValue vecFsqrt_2ops(
+      std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
+    const T* n = operands[0].getAsVector<T>();
+    T out[16 / sizeof(T)] = {0};
+    for (int i = 0; i < I; i++) {
+      out[i] = ::sqrt(n[i]);
+    }
+    return {out, 256};
+  }
+
   /** Helper function for NEON instructions with the format `ins vd[index],
    *  rn`.
    * T represents the vector register type (i.e. vd.16b would
--- src/lib/arch/aarch64/Instruction_execute.cc
+++ src/lib/arch/aarch64/Instruction_execute.cc
@@ -6380,16 +6380,16 @@ void Instruction::execute() {
       return executionNYI();
       break;
     }
-    case Opcode::AArch64_FSQRTv2f64: {
-      return executionNYI();
+    case Opcode::AArch64_FSQRTv2f64: {  // fsqrt vd.2d, vn.2d
+      results[0] = neonHelp::vecFsqrt_2ops<double, 2>(operands);
       break;
     }
     case Opcode::AArch64_FSQRTv4f16: {
       return executionNYI();
       break;
     }
-    case Opcode::AArch64_FSQRTv4f32: {
-      return executionNYI();
+    case Opcode::AArch64_FSQRTv4f32: {  // fsqrt vd.4s, vn.4s
+      results[0] = neonHelp::vecFsqrt_2ops<float, 4>(operands);
       break;
     }
     case Opcode::AArch64_FSQRTv8f16: {
