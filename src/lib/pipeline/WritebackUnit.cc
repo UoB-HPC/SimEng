947098bae24ee9e585db660e433a3f31b5eaeaba
jj16791 <jj16791@bristol.ac.uk>
2022-01-28 17:00:21 +0000

    InstructionsWritten_ is only incremented on the final micro-op to get correct retired stat from inorder core

--- src/lib/pipeline/WritebackUnit.cc
+++ src/lib/pipeline/WritebackUnit.cc
@@ -23,22 +23,40 @@ void WritebackUnit::tick() {
 
     auto& results = uop->getResults();
     auto& destinations = uop->getDestinationRegisters();
+    // std::cout << "WB: " << uop->getSequenceId() << ":"
+    //           << uop->getInstructionId() << ":0x" << std::hex
+    //           << uop->getInstructionAddress() << std::dec << ":"
+    //           << uop->getMicroOpIndex() << std::endl;
+    // std::cout << "WB: 0x" << std::hex << uop->getInstructionAddress()
+    //           << std::dec << std::endl;
     for (size_t i = 0; i < results.size(); i++) {
       // Write results to register file
       registerFileSet_.set(destinations[i], results[i]);
+      // std::cout << "\t" << unsigned(destinations[i].type) << ":"
+      //           << unsigned(destinations[i].tag) << " <- ";
+      // if (results[i].size() == 1)
+      //   std::cout << unsigned(results[i].get<uint8_t>());
+      // else if (results[i].size() == 2)
+      //   std::cout << results[i].get<uint16_t>();
+      // else if (results[i].size() == 4)
+      //   std::cout << results[i].get<uint32_t>();
+      // else if (results[i].size() == 8)
+      //   std::cout << results[i].get<uint64_t>();
+      // else if (results[i].size() == 256)
+      //   std::cout << results[i].getAsVector<uint64_t>()[0] << ":"
+      //             << results[i].getAsVector<uint64_t>()[1];
+      // else
+      //   std::cout << "N/A";
+      // std::cout << std::endl;
     }
     if (uop->isMicroOp()) {
       uop->setWaitingCommit();
       flagMicroOpCommits_(uop->getInstructionId());
+      if (uop->isLastMicroOp()) instructionsWritten_++;
     } else {
       uop->setCommitReady();
+      instructionsWritten_++;
     }
-
-    instructionsWritten_++;
-    std::cout << "WB: " << uop->getSequenceId() << ":"
-              << uop->getInstructionId() << ":0x" << std::hex
-              << uop->getInstructionAddress() << std::dec << ":"
-              << uop->getMicroOpIndex() << std::endl;
 
     completionSlots_[slot].getHeadSlots()[0] = nullptr;
   }
