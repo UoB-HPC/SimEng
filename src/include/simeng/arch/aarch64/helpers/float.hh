1058cad4eaed8fc97bf2219484430bb68266748e
Finn Wilkinson <finnwilkinson5@gmail.com>
2022-01-28 16:46:09 +0000

    Cleaned code in Float.hh helper function file.

--- src/include/simeng/arch/aarch64/helpers/float.hh
+++ src/include/simeng/arch/aarch64/helpers/float.hh
@@ -8,7 +8,8 @@ namespace aarch64 {
 class floatHelp {
  public:
   /** Helper function for instructions with the format `fabd rd, rn, rm`.
-   */
+   * T represents the type of operands (e.g. for sd T = float).
+   * Returns correctly formatted RegisterValue. */
   template <typename T>
   static RegisterValue fabd_3ops(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
@@ -17,8 +18,9 @@ class floatHelp {
     return {std::fabs(n - m), 256};
   }
 
-  /** Helper function for instructions with the format `fabs vd, vn`.
-   */
+  /** Helper function for instructions with the format `fabs rd, rn`.
+   * T represents the type of operands (e.g. for sd T = float).
+   * Returns correctly formatted RegisterValue. */
   template <typename T>
   static RegisterValue fabs_2ops(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
@@ -26,27 +28,10 @@ class floatHelp {
     return {std::fabs(n), 256};
   }
 
-  /** Helper function for instructions with the format `fcmp rn, <rm, #imm>`. */
-  template <typename T>
-  static uint8_t fcmp(
-      std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands,
-      bool useImm) {
-    T a = operands[0].get<T>();
-    // Dont need to fetch imm as will always be 0.0
-    T b = useImm ? 0 : operands[1].get<T>();
-    if (std::isnan(a) || std::isnan(b)) {
-      // TODO: Raise exception if NaNs are signalling or fcmpe
-      return AuxFunc::nzcv(false, false, true, true);
-    } else if (a == b) {
-      return AuxFunc::nzcv(false, true, true, false);
-    } else if (a < b) {
-      return AuxFunc::nzcv(true, false, false, false);
-    }
-    return AuxFunc::nzcv(false, false, true, false);
-  }
-
   /** Helper function for instructions with the format `fccmp rn, rm, #nzcv,
-   * cc`. */
+   * cc`.
+   * T represents the type of operands (e.g. for sn T = float).
+   * Returns single value of type uint8_t. */
   template <typename T>
   static uint8_t fccmp(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands,
@@ -68,8 +53,30 @@ class floatHelp {
     return static_cast<uint8_t>(metadata.operands[2].imm);
   }
 
+  /** Helper function for instructions with the format `fcmp rn, <rm, #imm>`.
+   * T represents the type of operands (e.g. for sn T = float).
+   * Returns single value of type uint8_t. */
+  template <typename T>
+  static uint8_t fcmp(
+      std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands,
+      bool useImm) {
+    T a = operands[0].get<T>();
+    // Dont need to fetch imm as will always be 0.0
+    T b = useImm ? 0 : operands[1].get<T>();
+    if (std::isnan(a) || std::isnan(b)) {
+      // TODO: Raise exception if NaNs are signalling or fcmpe
+      return AuxFunc::nzcv(false, false, true, true);
+    } else if (a == b) {
+      return AuxFunc::nzcv(false, true, true, false);
+    } else if (a < b) {
+      return AuxFunc::nzcv(true, false, false, false);
+    }
+    return AuxFunc::nzcv(false, false, true, false);
+  }
+
   /** Helper function for instructions with the format `fmaxnm rd, rn, rm`.
-   */
+   * T represents the type of operands (e.g. for sd T = float).
+   * Returns correctly formatted RegisterValue. */
   template <typename T>
   static RegisterValue fmaxnm_3ops(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
@@ -79,7 +86,8 @@ class floatHelp {
   }
 
   /** Helper function for instructions with the format `fmaxnm rd, rn, rm`.
-   */
+   * T represents the type of operands (e.g. for sd T = float).
+   * Returns correctly formatted RegisterValue. */
   template <typename T>
   static RegisterValue fminnm_3ops(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
@@ -89,7 +97,9 @@ class floatHelp {
   }
 
   /** Helper function for NEON instructions with the format `fnmsub rd, rn, rm,
-   * ra`. */
+   * ra`.
+   * T represents the type of operands (e.g. for sd T = float).
+   * Returns correctly formatted RegisterValue. */
   template <typename T>
   static RegisterValue fnmsub_4ops(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
