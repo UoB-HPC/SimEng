48fca720a6b641321832e17f622299212e77c12d
Finn Wilkinson <finnwilkinson5@gmail.com>
2022-01-28 16:53:52 +0000

    Cleaned code in Multiply.hh helper function file.

--- src/include/simeng/arch/aarch64/helpers/multiply.hh
+++ src/include/simeng/arch/aarch64/helpers/multiply.hh
@@ -8,7 +8,8 @@ namespace aarch64 {
 class multiplyHelp {
  public:
   /** Helper function for instructions with the format `madd rd, rn, rm, ra`.
-   * Returns single value. */
+   * T represents the type of operands (e.g. for xn, T = uint64_t).
+   * Returns single value of type T. */
   template <typename T>
   static T madd_4ops(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
@@ -22,7 +23,8 @@ class multiplyHelp {
    * D represents the type of the destination register (either int64_t or
    * uint64_t).
    * N represents the type of the first source register (either
-   * int32_t or uint32_t). Returns single value. */
+   * int32_t or uint32_t).
+   * Returns single value of type D. */
   template <typename D, typename N>
   static D maddl_4ops(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
@@ -33,7 +35,8 @@ class multiplyHelp {
   }
 
   /** Helper function for instructions with the format `mul rd, rn, rm`.
-   * Returns single value. */
+   * T represents the type of operands (e.g. for xn, T = uint64_t).
+   * Returns single value of type T. */
   template <typename T>
   static T mul_3ops(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
@@ -43,7 +46,8 @@ class multiplyHelp {
   }
 
   /** Helper function for instructions with the format `msub rd, rn, rm, ra`.
-   * Returns single value. */
+   * T represents the type of operands (e.g. for xn, T = uint64_t).
+   * Returns single value of type T. */
   template <typename T>
   static T msub_4ops(
       std::array<RegisterValue, Instruction::MAX_SOURCE_REGISTERS>& operands) {
