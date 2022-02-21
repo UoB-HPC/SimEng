821adef3efd90da8aa9596b0d9320447a351ed82
Finn Wilkinson <finnwilkinson5@gmail.com>
2022-02-21 11:51:02 +0000

    Fixed issue with LD1d (gather load, scaled) and ST1D (scatter store, scaled) sve instructions where the Z-reg memory operand was not detected correctly at decode.

--- src/lib/arch/aarch64/InstructionMetadata.cc
+++ src/lib/arch/aarch64/InstructionMetadata.cc
@@ -630,6 +630,7 @@ InstructionMetadata::InstructionMetadata(const cs_insn& insn)
     case Opcode::AArch64_GLD1D_SCALED_REAL:
       [[fallthrough]];
     case Opcode::AArch64_GLD1D_REAL: {
+      operandCount = 4;
       // LD1D gather instruction doesn't correctly identify destination
       // register
       uint16_t reg_enum = ARM64_REG_Z0;
@@ -647,6 +648,19 @@ InstructionMetadata::InstructionMetadata(const cs_insn& insn)
       // LD1D gather instruction doesn't correctly identify memory operands
       operands[2].type = ARM64_OP_MEM;
       operands[2].access = CS_AC_READ;
+      operands[3].type = ARM64_OP_MEM;
+      operands[3].access = CS_AC_READ;
+
+      // LD1D doesn't correctly identify vector memory register correctly
+      uint16_t vec_enum = ARM64_REG_Z0;
+      std::string tmp_str(operandStr.substr(operandStr.find("[")));
+      // Single or double digit Z register identifier
+      if (tmp_str.substr(tmp_str.find("z"))[2] == '.') {
+        vec_enum += std::stoi(tmp_str.substr(tmp_str.find("z") + 1, 1));
+      } else {
+        vec_enum += std::stoi(tmp_str.substr(tmp_str.find("z") + 1, 2));
+      }
+      operands[3].reg = static_cast<arm64_reg>(vec_enum);
       break;
     }
     case Opcode::AArch64_GLD1SW_D_IMM_REAL:
@@ -952,6 +966,7 @@ InstructionMetadata::InstructionMetadata(const cs_insn& insn)
     case Opcode::AArch64_SST1D:
       [[fallthrough]];
     case Opcode::AArch64_SST1D_SCALED: {
+      operandCount = 4;
       // ST1W doesn't correctly identify first source register
       uint16_t reg_enum = ARM64_REG_Z0;
       // Single or double digit Z register identifier
@@ -969,6 +984,19 @@ InstructionMetadata::InstructionMetadata(const cs_insn& insn)
       // operands
       operands[2].type = ARM64_OP_MEM;
       operands[2].access = CS_AC_READ;
+      operands[3].type = ARM64_OP_MEM;
+      operands[3].access = CS_AC_READ;
+
+      // ST1D doesn't correctly identify vector memory register correctly
+      uint16_t vec_enum = ARM64_REG_Z0;
+      std::string tmp_str(operandStr.substr(operandStr.find("[")));
+      // Single or double digit Z register identifier
+      if (tmp_str.substr(tmp_str.find("z"))[2] == '.') {
+        vec_enum += std::stoi(tmp_str.substr(tmp_str.find("z") + 1, 1));
+      } else {
+        vec_enum += std::stoi(tmp_str.substr(tmp_str.find("z") + 1, 2));
+      }
+      operands[3].reg = static_cast<arm64_reg>(vec_enum);
       break;
     }
     case Opcode::AArch64_ST2D_IMM: {
