72dca5eadfb6bcef65a220d56f3be40e0abf4ecf
Rahat <rahat.muneeb@hatless.studio>
2022-06-20 17:39:46 +0100

    implemented SST wrapper for SimEng core

---
+++ sst/SimengCoreWrapper.cc
@@ -0,0 +1,191 @@
+#include <sst/core/sst_config.h>
+
+#include "SimengCoreWrapper.hh"
+
+using namespace SST::SSTSimeng;
+
+enum class SimulationMode { Emulation, InOrderPipelined, OutOfOrder };
+
+SimengCoreWrapper::SimengCoreWrapper(SST::ComponentId_t id, SST::Params& params): SST::Component(id) {
+    output.init("SimengCoreWrapper[" + getName() + ":@p:@t]:", 999, 0, SST::Output::STDOUT);
+    clock = registerClock(params.find<std::string>("clock", "1GHz"), new SST::Clock::Handler<SimengCoreWrapper>(this, &SimengCoreWrapper::clockTick));
+
+    executable_path = params.find<std::string>("executable_path", "");
+    executable_args = params.find<std::string>("executable_args", "");
+
+    if (executable_path.length() == 0) {
+        output.fatal(CALL_INFO, 10, 0, "Simeng executable binary filepath not provided.");
+    }
+
+    iterations = 0;
+    vitrual_counter = 0;
+    size = 0;
+
+    registerAsPrimaryComponent();
+    primaryComponentDoNotEndSim();
+
+}
+
+SimengCoreWrapper::~SimengCoreWrapper() {}
+
+void SimengCoreWrapper::setup() {}
+
+void SimengCoreWrapper::finish() {
+
+    output.verbose(CALL_INFO, 1, 0, "Simulation complete. Finalising stats....\n");
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(endTime - start_time)
+          .count();
+    auto hz = iterations / (static_cast<double>(duration) / 1000.0);
+    auto khz = hz / 1000.0;
+    auto retired = core->getInstructionsRetiredCount();
+    auto mips = retired / static_cast<double>(duration) / 1000.0;
+
+  // Print stats
+    std::cout << "\n";
+    auto stats = core->getStats();
+    for (const auto& [key, value] : stats) {
+    std::cout << key << ": " << value << "\n";
+    }
+
+    std::cout << "\nFinished " << iterations << " ticks in " << duration << "ms ("
+            << std::round(khz) << " kHz, " << std::setprecision(2) << mips
+            << " MIPS)" << std::endl;
+
+    delete[] process_memory;
+}
+
+void SimengCoreWrapper::init(unsigned int phase) {
+    if (phase != 0) return;
+    fabricateSimengCore();
+}
+
+bool SimengCoreWrapper::clockTick(SST::Cycle_t current_cycle) {
+    // Tick the core and memory interfaces until the program has halted
+    if (!core->hasHalted() || data_memory->hasPendingRequests()) {
+    // Tick the core
+    core->tick();
+    // Update Virtual Counter Timer at correct frequency.
+    if (iterations % (uint64_t)timer_modulo == 0) {
+        vitrual_counter++;
+        core->incVCT(vitrual_counter);
+    }
+
+    // Tick memory
+    instruction_memory->tick();
+    data_memory->tick();
+
+    iterations++;
+
+    return false;
+    } else {
+        primaryComponentOKToEndSim();
+        return true;
+    }
+}
+
+void SimengCoreWrapper::fabricateSimengCore() {
+    output.output("Setting up Simeng core \n");
+
+    SimulationMode mode = SimulationMode::InOrderPipelined;
+    std::string modeString;
+    YAML::Node config;
+
+    if (config_path != "") {
+      config = simeng::ModelConfig(config_path).getConfigFile();
+    } else {
+      config = YAML::Load(DEFAULT_CONFIG);
+    }
+
+    if (config["Core"]["Simulation-Mode"].as<std::string>() == "inorder") {
+      mode = SimulationMode::InOrderPipelined;
+      modeString = "Emulation";
+    } else if (config["Core"]["Simulation-Mode"].as<std::string>() ==
+               "outoforder") {
+      mode = SimulationMode::OutOfOrder;
+      modeString = "Out-of-Order";
+    } else {
+      mode = SimulationMode::Emulation;
+      modeString = "Emulation";
+    }
+
+    float clockFreq_ = config["Core"]["Clock-Frequency"].as<float>();
+    uint32_t timerFreq_ = config["Core"]["Timer-Frequency"].as<uint32_t>();
+    timer_modulo = (clockFreq_ * 1e9) / (timerFreq_ * 1e6);
+
+    // Create the process Image
+    std::vector<std::string> commandLine({executable_path, executable_args});
+    process = std::make_unique<simeng::kernel::LinuxProcess>(commandLine, config);
+    if (!process->isValid()) output.fatal(CALL_INFO, 1, 0, "Could not read/parse %s", executable_path);
+
+    auto processImage = process->getProcessImage();
+    size_t processMemorySize = processImage.size();
+    process_memory = new char[processMemorySize]();
+    std::copy(processImage.begin(), processImage.end(), process_memory);
+
+    uint64_t entryPoint = process->getEntryPoint();
+
+    // Create the OS kernel with the process
+    kernel = std::make_unique<simeng::kernel::Linux>();
+    kernel->createProcess(*process.get());
+
+    instruction_memory = std::make_unique<simeng::FlatMemoryInterface>(
+        process_memory, processMemorySize);
+
+    // Create the architecture, with knowledge of the kernel
+    arch =
+        std::make_unique<simeng::arch::aarch64::Architecture>(*kernel, config);
+    
+    predictor = std::make_unique<simeng::BTBPredictor>(
+        config["Branch-Predictor"]["BTB-bitlength"].as<uint8_t>());
+    auto config_ports = config["Ports"];
+    std::vector<std::vector<uint16_t>> portArrangement(config_ports.size());
+    // Extract number of ports
+    for (size_t i = 0; i < config_ports.size(); i++) {
+      auto config_groups = config_ports[i]["Instruction-Group-Support"];
+      // Extract number of groups in port
+      for (size_t j = 0; j < config_groups.size(); j++) {
+        portArrangement[i].push_back(config_groups[j].as<uint16_t>());
+      }
+    }
+    port_allocator = std::make_unique<simeng::pipeline::BalancedPortAllocator>(
+        portArrangement);
+
+    // Configure reservation station arrangment
+    std::vector<std::pair<uint8_t, uint64_t>> rsArrangement;
+    for (size_t i = 0; i < config["Reservation-Stations"].size(); i++) {
+      auto reservation_station = config["Reservation-Stations"][i];
+      for (size_t j = 0; j < reservation_station["Ports"].size(); j++) {
+        uint8_t port = reservation_station["Ports"][j].as<uint8_t>();
+        if (rsArrangement.size() < port + 1) {
+          rsArrangement.resize(port + 1);
+        }
+        rsArrangement[port] = {i, reservation_station["Size"].as<uint16_t>()};
+      }
+    }
+
+    modeString = "Out-of-Order";
+    data_memory = std::make_unique<simeng::FixedLatencyMemoryInterface>(
+        process_memory, processMemorySize,
+        config["L1-Cache"]["Access-Latency"].as<uint16_t>());
+
+    core = std::make_unique<simeng::models::outoforder::Core>(
+        *instruction_memory, *data_memory, processMemorySize, entryPoint, *arch,
+        *predictor, *port_allocator, rsArrangement, config);
+
+    simeng::SpecialFileDirGen SFdir = simeng::SpecialFileDirGen(config);
+    // Create the Special Files directory if indicated to do so in Config
+    if (config["CPU-Info"]["Generate-Special-Dir"].as<std::string>() == "T") {
+        // Remove any current special files dir
+        SFdir.RemoveExistingSFDir();
+        // Create new special files dir
+        SFdir.GenerateSFDir();
+    }
+
+    output.verbose(CALL_INFO, 1, 0, "SimEng core setup successfully.\n");
+    std::cout << "Running in " << modeString << " mode\n";
+    std::cout << "Starting..." << std::endl;
+    start_time = std::chrono::high_resolution_clock::now();
+}
---
+++ sst/include/SimengCoreWrapper.hh
@@ -0,0 +1,88 @@
+#include <sst/core/sst_config.h>
+#include <sst/core/component.h>
+
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+#include "simeng/AlwaysNotTakenPredictor.hh"
+#include "simeng/BTBPredictor.hh"
+#include "simeng/BTB_BWTPredictor.hh"
+#include "simeng/Core.hh"
+#include "simeng/Elf.hh"
+#include "simeng/FixedLatencyMemoryInterface.hh"
+#include "simeng/FlatMemoryInterface.hh"
+#include "simeng/ModelConfig.hh"
+#include "simeng/SpecialFileDirGen.hh"
+#include "simeng/arch/Architecture.hh"
+#include "simeng/arch/aarch64/Architecture.hh"
+#include "simeng/arch/aarch64/Instruction.hh"
+#include "simeng/arch/aarch64/MicroDecoder.hh"
+#include "simeng/kernel/Linux.hh"
+#include "simeng/models/emulation/Core.hh"
+#include "simeng/models/inorder/Core.hh"
+#include "simeng/models/outoforder/Core.hh"
+#include "simeng/pipeline/A64FXPortAllocator.hh"
+#include "simeng/pipeline/BalancedPortAllocator.hh"
+#include "simeng/version.hh"
+
+using namespace SST;
+
+namespace SST {
+    namespace SSTSimeng {
+        class SimengCoreWrapper: public SST::Component {
+            public:
+                SimengCoreWrapper(SST::ComponentId_t id, SST::Params& params);
+                ~SimengCoreWrapper();
+
+                void setup();
+                void finish();
+
+                void init(unsigned int phase);
+                bool clockTick( SST::Cycle_t currentCycle );
+
+                SST_ELI_REGISTER_COMPONENT(
+                    SimengCoreWrapper,
+                    "sstsimeng",
+                    "simengcore",
+                    SST_ELI_ELEMENT_VERSION( 1, 0, 0 ),
+                    "Simeng core wrapper for SST",
+                    COMPONENT_CATEGORY_PROCESSOR
+                )
+
+                SST_ELI_DOCUMENT_PARAMS(
+                { "config", "Path to Simeng config file (YAML)", "../configs/a64fx.yaml" }
+                )
+            private:
+                // SST properties
+                SST::Output output;
+                TimeConverter* clock;
+                
+                // Simeng properties
+                std::unique_ptr<simeng::Core> core;
+                std::string config_path;
+                std::string executable_path;
+                std::string executable_args;
+                uint64_t cache_line_size;
+                std::unique_ptr<simeng::kernel::LinuxProcess> process;
+                std::unique_ptr<simeng::kernel::Linux> kernel;
+                char* process_memory;
+                std::unique_ptr<simeng::arch::Architecture> arch;
+                std::unique_ptr<simeng::MemoryInterface> instruction_memory;
+                std::unique_ptr<simeng::BranchPredictor> predictor;
+                std::unique_ptr<simeng::pipeline::PortAllocator> port_allocator;
+                // Replace with SST memory model
+                std::unique_ptr<simeng::MemoryInterface> data_memory;
+                int iterations;
+                int vitrual_counter;
+                double timer_modulo;
+                int size;
+                std::chrono::high_resolution_clock::time_point start_time;
+
+                void fabricateSimengCore();
+        };
+    }
+}
