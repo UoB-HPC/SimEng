ad9c1d8944ba84eb2f75aafb6e654fcf8b215d62
Thomas Hepworth <tomhepworth@hotmail.co.uk>
2022-06-25 01:10:48 +0100

    AArch64_LD1Onev16_POST instruciton and test

--- src/lib/arch/aarch64/Instruction_address.cc
+++ src/lib/arch/aarch64/Instruction_address.cc
@@ -178,6 +178,11 @@ span<const MemoryAccessTarget> Instruction::generateAddresses() {
         setMemoryAddresses({{base, 16}});
         break;
       }
+      case Opcode::AArch64_LD1Onev16b_POST: {  // ld1 {vt.16b}, [xn], #imm
+        uint64_t base = operands[0].get<uint64_t>();
+        setMemoryAddresses({{base, 16}});
+        break;
+      }
       case Opcode::AArch64_LD1Twov16b: {  // ld1 {vt1.16b, vt2.16b}, [xn]
         uint64_t base = operands[0].get<uint64_t>();
         setMemoryAddresses({{base, 16}, {base + 16, 16}});
