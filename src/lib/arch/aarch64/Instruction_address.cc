60cf89079e0ec0e38e50386eda631905acc770d1
Thomas Hepworth <tomhepworth@hotmail.co.uk>
2022-08-13 20:15:58 +0100

    Clang-format changes

--- src/lib/arch/aarch64/Instruction_address.cc
+++ src/lib/arch/aarch64/Instruction_address.cc
@@ -423,7 +423,7 @@ span<const MemoryAccessTarget> Instruction::generateAddresses() {
         setMemoryAddresses(std::move(addresses));
         break;
       }
-      case Opcode::AArch64_LD2Twov4s: // ld2 {vt1.4s, vt2.4s}, [xn]
+      case Opcode::AArch64_LD2Twov4s:  // ld2 {vt1.4s, vt2.4s}, [xn]
         [[fallthrough]];
       case Opcode::AArch64_LD2Twov4s_POST: {  // ld2 {vt1.4s, vt2.4s}, [xn],
                                               // #imm
