568e87fcfda2f616da9370dde54875ec100a7483
Finn Wilkinson <finnwilkinson5@gmail.com>
2022-08-17 17:22:56 +0100

    Corrected Matrix Reg size and altered how it is represented (now 256 256-byte vectors, each representing a row).

--- src/lib/arch/aarch64/Architecture.cc
+++ src/lib/arch/aarch64/Architecture.cc
@@ -241,7 +241,7 @@ std::vector<RegisterFileStructure> Architecture::getRegisterFileStructures()
       {32, 17},         // Predicate
       {1, 1},           // NZCV
       {8, numSysRegs},  // System
-      {512 * 512, 1},   // Matrix
+      {256, 256},       // Matrix (Each row is a register)
   };
 }
 
