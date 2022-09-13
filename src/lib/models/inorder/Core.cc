19c05cba329efe602424a7313dcbbda541c732a3
jj16791 <32519988+jj16791@users.noreply.github.com>
2022-09-13 09:56:40 +0100

    Prevent ExceptionHandler processing if there are active memory requests (#235)

--- src/lib/models/inorder/Core.cc
+++ src/lib/models/inorder/Core.cc
@@ -192,6 +192,11 @@ void Core::handleException() {
 void Core::processExceptionHandler() {
   assert(exceptionHandler_ != nullptr &&
          "Attempted to process an exception handler that wasn't present");
+  if (dataMemory_.hasPendingRequests()) {
+    // Must wait for all memory requests to complete before processing the
+    // exception
+    return;
+  }
 
   auto success = exceptionHandler_->tick();
   if (!success) {
