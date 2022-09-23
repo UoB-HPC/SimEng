9b42a258c8101dfa593d199ca424dfdc7410c9e5
rahahahat <62028695+rahahahat@users.noreply.github.com>
2022-09-23 15:53:18 +0100

    Initial SST integration with SimEng and conditional compilation using cmake. (#231)
    This PR implements the `SST` wrapper and `simeng::MemoryInterface` needed for successful integration with SST. It also adds support for conditional compilation through `CMake`.

---
+++ sst/SimEngCoreWrapper.cc
@@ -0,0 +1,285 @@
+// clang-format off
+// DO NOT MOVE FROM TOP OF FILE - https://github.com/sstsimulator/sst-core/issues/865
+#include <sst/core/sst_config.h>
+// clang-format on
+
+#include "SimEngCoreWrapper.hh"
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace SST::SSTSimEng;
+using namespace SST::Interfaces;
+
+SimEngCoreWrapper::SimEngCoreWrapper(SST::ComponentId_t id, SST::Params& params)
+    : SST::Component(id) {
+  output_.init("SimEngCoreWrapper[" + getName() + ":@p:@t]:", 999, 0,
+               SST::Output::STDOUT);
+  clock_ = registerClock(params.find<std::string>("clock", "1GHz"),
+                         new SST::Clock::Handler<SimEngCoreWrapper>(
+                             this, &SimEngCoreWrapper::clockTick));
+
+  // Extract variables from config.py
+  executablePath_ = params.find<std::string>("executable_path", "");
+  executableArgs_ = splitArgs(params.find<std::string>("executable_args", ""));
+  simengConfigPath_ = params.find<std::string>("simeng_config_path", "");
+  cacheLineWidth_ = params.find<uint64_t>("cache_line_width", "64");
+  maxAddrMemory_ = params.find<uint64_t>("max_addr_memory", "0");
+
+  if (executablePath_.length() == 0) {
+    output_.fatal(CALL_INFO, 10, 0,
+                  "SimEng executable binary filepath not provided.");
+  }
+  if (maxAddrMemory_ == 0) {
+    output_.fatal(CALL_INFO, 10, 0,
+                  "Maximum address range for memory not provided");
+  }
+
+  iterations_ = 0;
+
+  // Instantiate the StandardMem Interface defined in config.py
+  sstMem_ = loadUserSubComponent<SST::Interfaces::StandardMem>(
+      "memory", ComponentInfo::SHARE_NONE, clock_,
+      new StandardMem::Handler<SimEngCoreWrapper>(
+          this, &SimEngCoreWrapper::handleMemoryEvent));
+
+  dataMemory_ = std::make_shared<SimEngMemInterface>(sstMem_, cacheLineWidth_,
+                                                     maxAddrMemory_);
+
+  handlers_ = new SimEngMemInterface::SimEngMemHandlers(*dataMemory_, &output_);
+
+  // Protected methods from SST::Component used to start simulation
+  registerAsPrimaryComponent();
+  primaryComponentDoNotEndSim();
+}
+
+SimEngCoreWrapper::~SimEngCoreWrapper() {}
+
+void SimEngCoreWrapper::setup() {
+  sstMem_->setup();
+  output_.verbose(CALL_INFO, 1, 0, "Memory setup complete\n");
+}
+
+void SimEngCoreWrapper::handleMemoryEvent(StandardMem::Request* memEvent) {
+  memEvent->handle(handlers_);
+}
+
+void SimEngCoreWrapper::finish() {
+  output_.verbose(CALL_INFO, 1, 0,
+                  "Simulation complete. Finalising stats....\n");
+
+  auto endTime = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+                      endTime - startTime_)
+                      .count();
+  double khz =
+      (iterations_ / (static_cast<double>(duration) / 1000.0)) / 1000.0;
+  uint64_t retired = core_->getInstructionsRetiredCount();
+  double mips = retired / (static_cast<double>(duration) / 1000.0);
+
+  // Print stats
+  std::cout << "\n";
+  auto stats = core_->getStats();
+  for (const auto& [key, value] : stats) {
+    std::cout << key << ": " << value << "\n";
+  }
+
+  std::cout << "\nFinished " << iterations_ << " ticks in " << duration
+            << "ms (" << std::round(khz) << " kHz, " << std::setprecision(2)
+            << mips << " MIPS)" << std::endl;
+}
+
+void SimEngCoreWrapper::init(unsigned int phase) {
+  sstMem_->init(phase);
+  // Init can have multiple phases, only fabricate the core once at phase 0
+  if (phase == 0) {
+    fabricateSimEngCore();
+  }
+}
+
+bool SimEngCoreWrapper::clockTick(SST::Cycle_t current_cycle) {
+  // Tick the core and memory interfaces until the program has halted
+  if (!core_->hasHalted() || dataMemory_->hasPendingRequests()) {
+    // Tick the core
+    core_->tick();
+
+    // Tick memory
+    instructionMemory_->tick();
+    dataMemory_->tick();
+
+    iterations_++;
+
+    return false;
+  } else {
+    // Protected method from SST::Component used to end SST simulation
+    primaryComponentOKToEndSim();
+    return true;
+  }
+}
+std::string SimEngCoreWrapper::trimSpaces(std::string strArgs) {
+  int trailingEnd = -1;
+  int leadingEnd = -1;
+  for (int x = 0; x < strArgs.size(); x++) {
+    int end = strArgs.size() - 1 - x;
+    // Find the index, from the start of the string, which is not a space.
+    if (strArgs.at(x) != ' ' && leadingEnd == -1) {
+      leadingEnd = x;
+    }
+    // Find the index, from the end of the string, which is not a space.
+    if (strArgs.at(end) != ' ' && trailingEnd == -1) {
+      trailingEnd = end;
+    }
+    if (trailingEnd != -1 && leadingEnd != -1) {
+      break;
+    }
+  }
+  // The string has leading or trailing spaces, return the substring which
+  // doesn't have those spaces.
+  if (trailingEnd != -1 && leadingEnd != -1) {
+    return strArgs.substr(leadingEnd, trailingEnd - leadingEnd + 1);
+  }
+  // The string does not have leading or trailing spaces, return the original
+  // string.
+  return strArgs;
+};
+
+std::vector<std::string> SimEngCoreWrapper::splitArgs(std::string strArgs) {
+  std::string trimmedStrArgs = trimSpaces(strArgs);
+  std::string str = "";
+  std::vector<std::string> args;
+  std::size_t argSize = trimmedStrArgs.size();
+  bool escapeSingle = false;
+  bool escapeDouble = false;
+  bool captureEscape = false;
+
+  if (argSize == 0) {
+    return args;
+  }
+
+  for (int x = 0; x < argSize; x++) {
+    bool escaped = escapeDouble || escapeSingle;
+    char currChar = trimmedStrArgs.at(x);
+    if (captureEscape) {
+      captureEscape = false;
+      str += currChar;
+    }
+    // This if statement check for an escaped '\' in the string.
+    // Any character after the '\' is appended to the current argument,
+    // without any delimiting or escape behaviour.
+    else if (currChar == '\\') {
+      captureEscape = true;
+    } else if (escaped) {
+      // If a portion of the argument string starts with a single quote (") and
+      // we encounter another single quote, capture the substring enclosed by a
+      // valid set of single quotes into an argument without producing any
+      // delimiting or escape behavior even with double quotes.
+      // e.g "arg1=1 arg2='"Hi"' arg3=2" will be parsed as
+      // std::vector<std::string>{arg1=1, arg2="Hi", arg3=2}
+      if (currChar == '\'' && escapeSingle) {
+        escapeSingle = 0;
+      }
+      // If a portion of the argument string starts with a double quote (") and
+      // we encounter another double quote, capture the substring enclosed by a
+      // valid set of double quotes into an argument without producing any
+      // delimiting or escape behavior even with single quotes.
+      // e.g "arg1=1 arg2="James' Car" arg3=2" will be parsed as
+      // std::vector<std::string>{arg1=1, arg2=James' Car, arg3=2}
+      else if (currChar == '\"' && escapeDouble) {
+        escapeDouble = 0;
+      } else {
+        str += currChar;
+      }
+    } else {
+      if (currChar == ' ') {
+        if (str != "") {
+          args.push_back(str);
+          str = "";
+        }
+      }
+      // Check for escape character ("), this signals the algorithm to capture
+      // any char inside a set of ("") without producing any delimiting or
+      // escape behavior.
+      else if (currChar == '\"') {
+        escapeDouble = 1;
+        // Check for escape character ('), this signals the algorithm to capture
+        // any char inside a set of ('') without producing any delimiting or
+        // escape behavior.
+      } else if (currChar == '\'') {
+        escapeSingle = 1;
+      } else {
+        str += currChar;
+      }
+    }
+  }
+  if (escapeSingle || escapeDouble) {
+    std::cerr << "Parsing failed: Invalid format - Please make sure all "
+                 "characters/strings are escaped properly."
+              << std::endl;
+    exit(1);
+  }
+  args.push_back(str);
+  return args;
+}
+
+void SimEngCoreWrapper::fabricateSimEngCore() {
+  output_.verbose(CALL_INFO, 1, 0, "Setting up SimEng Core\n");
+
+  // Create the instance of the core to be simulated
+  if (simengConfigPath_ != "") {
+    coreInstance_ = std::make_unique<simeng::CoreInstance>(
+        simengConfigPath_, executablePath_, executableArgs_);
+  } else {
+    coreInstance_ = std::make_unique<simeng::CoreInstance>(executablePath_,
+                                                           executableArgs_);
+  }
+
+  // Set the SST data memory SimEng should use
+  coreInstance_->setL1DataMemory(dataMemory_);
+
+  // Construct core
+  coreInstance_->createCore();
+
+  // Get remaining simulation objects needed to forward simulation
+  core_ = coreInstance_->getCore();
+  instructionMemory_ = coreInstance_->getInstructionMemory();
+
+  // This check ensures that SST has enough memory to store the entire
+  // processImage constructed by SimEng.
+  if (maxAddrMemory_ < coreInstance_->getProcessImageSize()) {
+    output_.verbose(
+        CALL_INFO, 1, 0,
+        "Error: SST backend memory is less than processImage size. Please "
+        "increase the memory allocated to memHierarchy.memBackend and "
+        "ensure it is consistent with \'max_addr_memory\' and "
+        "\'addr_range_end\'. \n");
+    primaryComponentOKToEndSim();
+    std::exit(EXIT_FAILURE);
+  }
+
+  // Send the process image data over to the SST memory
+  dataMemory_->sendProcessImageToSST(coreInstance_->getProcessImage().get(),
+                                     coreInstance_->getProcessImageSize());
+
+  output_.verbose(CALL_INFO, 1, 0, "SimEng core setup successfully.\n");
+  // Print out build metadata
+  std::cout << "[SimEng] Build metadata:" << std::endl;
+  std::cout << "[SimEng] \tVersion: " SIMENG_VERSION << std::endl;
+  std::cout << "[SimEng] \tCompile Time - Date: " __TIME__ " - " __DATE__
+            << std::endl;
+  std::cout << "[SimEng] \tBuild type: " SIMENG_BUILD_TYPE << std::endl;
+  std::cout << "[SimEng] \tCompile options: " SIMENG_COMPILE_OPTIONS
+            << std::endl;
+  std::cout << "[SimEng] \tTest suite: " SIMENG_ENABLE_TESTS << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "[SimEng] Running in "
+            << coreInstance_->getSimulationModeString() << " mode" << std::endl;
+  std::cout << "[SimEng] Workload: " << executablePath_;
+  for (const auto& arg : executableArgs_) std::cout << " " << arg;
+  std::cout << std::endl;
+  std::cout << "[SimEng] Config file: " << simengConfigPath_ << std::endl;
+
+  // Run simulation
+  std::cout << "[SimEng] Starting...\n" << std::endl;
+  startTime_ = std::chrono::high_resolution_clock::now();
+}
---
+++ sst/include/SimEngCoreWrapper.hh
@@ -0,0 +1,190 @@
+// clang-format off
+// DO NOT MOVE FROM TOP OF FILE - https://github.com/sstsimulator/sst-core/issues/865
+#include <sst/core/sst_config.h>
+// clang-format on
+#include <sst/core/component.h>
+#include <sst/core/eli/elementinfo.h>
+#include <sst/core/interfaces/stdMem.h>
+
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "SimEngMemInterface.hh"
+#include "simeng/Core.hh"
+#include "simeng/CoreInstance.hh"
+#include "simeng/MemoryInterface.hh"
+#include "simeng/SpecialFileDirGen.hh"
+#include "simeng/version.hh"
+
+using namespace SST;
+using namespace SST::Interfaces;
+using namespace SST::SSTSimEng;
+using namespace simeng;
+
+namespace SST {
+
+namespace SSTSimEng {
+
+/**
+ * A Wrapper class registered as a custom SST::Component to participate in an
+ * SST simulation. The SimEng core as well as componets/interfaces from SST
+ * required to ensure a succesful integration are instantiated and configured in
+ * this class as well. This class acts as the point of main contact for clock
+ * ticks recieved from SST and hence is also responsible for ticking the SimEng
+ * core and other classes assosciated to it.
+ */
+class SimEngCoreWrapper : public SST::Component {
+ public:
+  SimEngCoreWrapper(SST::ComponentId_t id, SST::Params& params);
+  ~SimEngCoreWrapper();
+
+  /** SST lifecycle methods (in-order of invocation) overriden from
+   * SST::Component. */
+
+  /**
+   * This is the init lifecycle method present in all SST::Components.
+   * Here it is overriden to include init calls to all other SST::Components
+   * which are contained inside SimEngCoreWrapper. It is neccessary to call all
+   * lifecycle methods for SST::Component(s).
+   */
+  void init(unsigned int phase);
+
+  /**
+   * This is the setup lifecycle method present in all SST::Components.
+   * Here it is overriden to include setup calls to all other SST::Components
+   * which are contained inside SimEngCoreWrapper. It is neccessary to call all
+   * lifecycle methods for SST::Component(s).
+   */
+  void setup();
+
+  /**
+   * This is the finish lifecycle method present in all SST::Components.
+   * Here it is overriden to finish statistics about the SimEng simulation.
+   */
+  void finish();
+
+  /**
+   * The clockTick is a method present in all SST::Components. This fuction
+   * is called everytime the SST clock ticks. The current clock cycle is passed
+   * as an argument by SST. The SimEng core ticks in this method.
+   */
+  bool clockTick(SST::Cycle_t currentCycle);
+
+  /**
+   * This handle event method is registered to StandardMem interface. This
+   * method is called everytime a memory request is forwarded by the interface.
+   * This function acts as a callback and invokes SimEngMemHandler on the memory
+   * requests.
+   */
+  void handleMemoryEvent(StandardMem::Request* memEvent);
+
+  /**
+   * SST supplied MACRO used to register custom SST:Components with
+   * the SST Core.
+   */
+  SST_ELI_REGISTER_COMPONENT(SimEngCoreWrapper, "sstsimeng", "simengcore",
+                             SST_ELI_ELEMENT_VERSION(1, 0, 0),
+                             "SimEng core wrapper for SST",
+                             COMPONENT_CATEGORY_PROCESSOR)
+
+  /**
+   * SST supplied MACRO used to document all parameters needed by
+   * a custom SST:Component.
+   */
+  SST_ELI_DOCUMENT_PARAMS(
+      {"simeng_config_path", "Path to SimEng YAML model config file (string)",
+       ""},
+      {"executable_path",
+       "Path to executable binary to be run by SimEng (string)", ""},
+      {"executable_args",
+       "argument to be passed to the executable binary (string)", ""},
+      {"clock", "Clock rate of the SST clock (string)", ""},
+      {"max_addr_memory", "Maximum address that memory can access (int)"},
+      {"cache_line_width", "The width of the cache line in bytes. (int)"}, )
+
+ private:
+  /** Method used to assemble SimEng core. */
+  void fabricateSimEngCore();
+
+  /** Method to split the passed executable argument's string into a vector of
+   * individual arguments. */
+  std::vector<std::string> splitArgs(std::string argString);
+
+  /** This method trims any leading or trailing spaces in a string. */
+  std::string trimSpaces(std::string argsStr);
+
+  // SST properties
+  /**
+   * SST defined output class used to output information to standard output.
+   * This class has in-built method for different levels of severity and can
+   * also be configured to output information like line-number and filename.
+   */
+  SST::Output output_;
+
+  /**
+   * SST clock for the component register with the custom component
+   * during instantiation using the registerClock method provided
+   * by SST.
+   */
+  TimeConverter* clock_;
+
+  /**
+   * SST::Interfaces::StandardMem interface responsible for converting
+   * SST::StandardMem::Request(s) into SST memory events to be passed
+   * down the memory heirarchy.
+   */
+  StandardMem* sstMem_;
+
+  // SimEng properties
+  /** Reference to the CoreInstance class responsible for creating the core to
+   * be simulated. */
+  std::unique_ptr<simeng::CoreInstance> coreInstance_;
+
+  /** Reference to SimEng core. */
+  std::shared_ptr<simeng::Core> core_;
+
+  /** Path to the YAML configuration file for SimEng. */
+  std::string simengConfigPath_;
+
+  /** Path to the executable binary to be simulated by SimEng. */
+  std::string executablePath_;
+
+  /** Arguments to be passed to executable binary. */
+  std::vector<std::string> executableArgs_;
+
+  /** The cache line width for SST. */
+  uint64_t cacheLineWidth_;
+
+  /** Maximum address availbale to SimEng for memory purposes. */
+  uint64_t maxAddrMemory_;
+
+  /** Reference to the process memory used in SimEng. */
+  std::shared_ptr<char> processMemory_;
+
+  /** Reference to SimEng instruction memory. */
+  std::shared_ptr<simeng::MemoryInterface> instructionMemory_;
+
+  /** Reference to SimEngMemInterface used for interfacing with SST. */
+  std::shared_ptr<SimEngMemInterface> dataMemory_;
+
+  /** Number of clock iterations. */
+  int iterations_;
+
+  /** Start time of simulation. */
+  std::chrono::high_resolution_clock::time_point startTime_;
+
+  /** Reference to memory request handler class defined in SimEngMemInterface.
+   */
+  SimEngMemInterface::SimEngMemHandlers* handlers_;
+};
+
+}  // namespace SSTSimEng
+
+}  // namespace SST
