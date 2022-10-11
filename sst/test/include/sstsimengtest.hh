6de26e3ebe55efd895638232933d565d896bd4be
Rahat <rahat.muneeb@hatless.studio>
2022-10-11 12:25:07 +0100

    Removed RUN_ALL_TESTS MACRO and moved logic to main.cc - SE-121

--- sst/test/include/sstsimengtest.hh
+++ sst/test/include/sstsimengtest.hh
@@ -3,19 +3,3 @@
 #include "framework/registry.hh"
 #include "framework/runner.hh"
 #include "framework/stats.hh"
-
-#define RUN_ALL_TESTS()                                          \
-  Registry* reg = Registry::getInstance();                       \
-  auto map = reg->getMap();                                      \
-  for (auto itr = map->begin(); itr != map->end(); itr++) {      \
-    std::vector<Registry::Factory> fvec = itr->second;           \
-    for (auto itrr = fvec.begin(); itrr != fvec.end(); itrr++) { \
-      std::unique_ptr<Runner> rn = (*itrr)();                    \
-      rn->run();                                                 \
-    }                                                            \
-  }                                                              \
-  auto& stats = Stats::getInstance();                            \
-  stats->printStats();                                           \
-  if (stats->getFailureCount() > 0) {                            \
-    exit(EXIT_FAILURE);                                          \
-  }
--- sst/test/main.cc
+++ sst/test/main.cc
@@ -1,3 +1,18 @@
 #include "sstsimengtest.hh"
 
-int main(void) { RUN_ALL_TESTS() }
+int main(void) {
+  Registry* reg = Registry::getInstance();
+  auto map = reg->getMap();
+  for (auto itr = map->begin(); itr != map->end(); itr++) {
+    std::vector<Registry::Factory> fvec = itr->second;
+    for (auto itrr = fvec.begin(); itrr != fvec.end(); itrr++) {
+      std::unique_ptr<Runner> rn = (*itrr)();
+      rn->run();
+    }
+  }
+  auto& stats = Stats::getInstance();
+  stats->printStats();
+  if (stats->getFailureCount() > 0) {
+    exit(EXIT_FAILURE);
+  }
+}
