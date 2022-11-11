5f719c5b708c60ce95b0d99d454243af8f5bcd03
Rahat <rahat.muneeb@hatless.studio>
2022-11-11 12:33:19 +0000

    Added larc A and C model SST configs and made changes to a64fx model config

--- sst/config/larc-c-config.py
+++ sst/config/larc-c-config.py
@@ -31,45 +31,45 @@ def getMemoryProps(memory_size: int, si: str):
 
 
 
-# ------------------------------------------- A64FX Properties ---------------------------------------
+# ------------------------------------------- LARC Properties ---------------------------------------
 
 # This SST configuration file represents the memory model for the Fujitsu A64fx processor.
-# Reference: https://github.com/fujitsu/A64FX/blob/master/doc/A64FX_Microarchitecture_Manual_en_1.8.pdf
+# Reference: https://github.com/fujitsu/LARC/blob/master/doc/LARC_Microarchitecture_Manual_en_1.8.pdf
 
-# Cache line size of L1 & L2 in A64FX in bytes.
-A64FX_CLW = 256
-# Clock Frequency of A64FX.
-A64FX_CLOCK = "1.8GHz"
-# Size of L1 cache in A64fx.
-A64FX_L1_SIZE = "64KiB"
-# Size of L2 cache in A64fx.
-A64FX_L2_SIZE = "8MiB"
-# Set associativity of A64FX L1
-A64FX_SA_L1 = 4
-# Set associativity of A64FX L2
-A64FX_SA_L2 = 16
-# Hit latency of A64FX L1 cache (cycles).
-A64FX_HL_L1 = 5
-# Hit latency of A64FX L2 cache (cycles).
-A64FX_HL_L2 = 42
-# Cohenrence protocol of A64FX caches.
-A64FX_COHP = "MESI"
-# L1 & L2 cache type of A64FX.
-A64FX_CACHE_TYPE = "inclusive"
-# Throughput of L1 to L2 per core in A64FX. (bytes per cycle)
-A64FX_L1TOL2_PC_TPUT = "32B"
-# Throughput of L1 to CPU per core in A64FX. Value of 0 indicates infinity. (bytes per cycle)
-A64FX_L1TOCPU_PC_TPUT = "128B"
-# Throughput of L2 to Memory per CMG in A64FX. (bytes per cycle)
-A64FX_L2TOMEM_PCMG_TPUT = "64B"
-# Throughput of L2 to L1 per core in A64FX. (bytes per cycle)
-A64FX_L2TOL1_PC_TPUT = "64B"
-# Throughput of Memory to L2 per CMG in A64FX. (bytes per cycle)
-A64FX_MEMTOL2_PCMG_TPUT = 128
-# A64FX Memory access time.
-A64FX_MEM_ACCESS = "144.5ns"
+# Cache line size of L1 & L2 in LARC in bytes.
+LARC_CLW = 256
+# Clock Frequency of LARC.
+LARC_CLOCK = "2.2GHz"
+# Size of L1 cache in Larc.
+LARC_L1_SIZE = "64KiB"
+# Size of L2 cache in Larc.
+LARC_L2_SIZE = "256MiB"
+# Set associativity of LARC L1
+LARC_SA_L1 = 4
+# Set associativity of LARC L2
+LARC_SA_L2 = 16
+# Hit latency of LARC L1 cache (cycles).
+LARC_HL_L1 = 3
+# Hit latency of LARC L2 cache (cycles).
+LARC_HL_L2 = 37
+# Cohenrence protocol of LARC caches.
+LARC_COHP = "MESI"
+# L1 & L2 cache type of LARC.
+LARC_CACHE_TYPE = "inclusive"
+# Throughput of L1 to L2 per core in LARC. (bytes per cycle)
+LARC_L1TOL2_PC_TPUT = "32B"
+# Throughput of L1 to CPU per core in LARC. Value of 0 indicates infinity. (bytes per cycle)
+LARC_L1TOCPU_PC_TPUT = "128B"
+# Throughput of L2 to Memory per CMG in LARC. (bytes per cycle)
+LARC_L2TOMEM_PCMG_TPUT = "64B"
+# Throughput of L2 to L1 per core in LARC. (bytes per cycle)
+LARC_L2TOL1_PC_TPUT = "64B"
+# Throughput of Memory to L2 per CMG in LARC. (bytes per cycle)
+LARC_MEMTOL2_PCMG_TPUT = 128
+# LARC Memory access time.
+LARC_MEM_ACCESS = "144.5ns"
 
-# ------------------------------------------- A64FX Properties ---------------------------------------
+# ------------------------------------------- LARC Properties ---------------------------------------
 
 
 # ---------------------------------------------- Variables -------------------------------------------
@@ -84,12 +84,12 @@ memprops = getMemoryProps(8, "GiB")
 # Using sst-info sstsimeng.simengcore to get all cache parameters, ports and subcomponent slots.
 cpu = sst.Component("core", "sstsimeng.simengcore")
 cpu.addParams({
-    "simeng_config_path": "/home/rahat/asimov/SimEng/configs/sst-cores/a64fx-sst.yaml",
-    "executable_path": "/home/rahat/asimov/ssh-dir/cachebw_static",
-    "executable_args": "64 1000",
-    "clock" : A64FX_CLOCK,
+    "simeng_config_path": "<PATH TO LARC SIMENG MODEL CONFIG>",
+    "executable_path": "<PATH TO EXECUTABLE BINARY>",
+    "executable_args": "",
+    "clock" : LARC_CLOCK,
     "max_addr_memory": memprops["end_addr"],
-    "cache_line_width": A64FX_CLW,
+    "cache_line_width": LARC_CLW,
 })
 
 # Instantiating the StandardInterface which communicates with the SST memory model.
@@ -101,20 +101,20 @@ interface = cpu.setSubComponent("memory", "memHierarchy.standardInterface")
 # --------------------------------------------- L1 Cache ---------------------------------------------
 
 # Using sst-info memHierarchy.Cache to get all cache parameters, ports and subcomponent slots.
-l1cache = sst.Component("a64fx.l1cache", "memHierarchy.Cache")
+l1cache = sst.Component("larc.l1cache", "memHierarchy.Cache")
 l1cache.addParams({
       "L1" : 1,
-      "cache_type": A64FX_CACHE_TYPE,
-      "access_latency_cycles" : A64FX_HL_L1,
-      "cache_frequency" : A64FX_CLOCK,
-      "associativity" : A64FX_SA_L1,
-      "cache_line_size" : A64FX_CLW,
-      "cache_size" : A64FX_L1_SIZE,
+      "cache_type": LARC_CACHE_TYPE,
+      "access_latency_cycles" : LARC_HL_L1,
+      "cache_frequency" : LARC_CLOCK,
+      "associativity" : LARC_SA_L1,
+      "cache_line_size" : LARC_CLW,
+      "cache_size" : LARC_L1_SIZE,
       "debug" : DEBUG_L1,
       "debug_level" : DEBUG_LEVEL,
-      "coherence_protocol": A64FX_COHP,
-      "request_link_width": A64FX_L1TOL2_PC_TPUT,
-      "response_link_width": A64FX_L1TOCPU_PC_TPUT
+      "coherence_protocol": LARC_COHP,
+      "request_link_width": LARC_L1TOL2_PC_TPUT,
+      "response_link_width": LARC_L1TOCPU_PC_TPUT
 })
 # Set MESI L1 coherence controller to the "coherence" slot
 coherence_controller_l1 = l1cache.setSubComponent("coherence", "memHierarchy.coherence.mesi_l1")
@@ -128,20 +128,20 @@ replacement_policy_l1 = l1cache.setSubComponent("replacement", "memHierarchy.rep
 # --------------------------------------------- L2 Cache ---------------------------------------------
 
 # Using sst-info memHierarchy.Cache to get all cache parameters, ports and subcomponent slots.
-l2cache = sst.Component("a64fx.l2cache", "memHierarchy.Cache")
+l2cache = sst.Component("larc.l2cache", "memHierarchy.Cache")
 l2cache.addParams({
       "L1" : 0,
-      "cache_type": A64FX_CACHE_TYPE,
-      "access_latency_cycles" : A64FX_HL_L2,
-      "cache_frequency" : A64FX_CLOCK,
-      "associativity" : A64FX_SA_L2,
-      "cache_line_size" : A64FX_CLW,
-      "cache_size" : A64FX_L2_SIZE,
+      "cache_type": LARC_CACHE_TYPE,
+      "access_latency_cycles" : LARC_HL_L2,
+      "cache_frequency" : LARC_CLOCK,
+      "associativity" : LARC_SA_L2,
+      "cache_line_size" : LARC_CLW,
+      "cache_size" : LARC_L2_SIZE,
       "debug" : DEBUG_L2,
       "debug_level" : DEBUG_LEVEL,
-      "coherence_protocol": A64FX_COHP,
-      "request_link_width": A64FX_L2TOMEM_PCMG_TPUT,
-      "response_link_width": A64FX_L2TOL1_PC_TPUT,
+      "coherence_protocol": LARC_COHP,
+      "request_link_width": LARC_L2TOMEM_PCMG_TPUT,
+      "response_link_width": LARC_L2TOL1_PC_TPUT,
 })
 # Set MESI L2 coherence controller to the "coherence" slot
 coherence_controller_l2 = l2cache.setSubComponent("coherence", "memHierarchy.coherence.mesi_inclusive")
@@ -154,11 +154,11 @@ replacement_policy_l2 = l2cache.setSubComponent("replacement", "memHierarchy.rep
 
 # ----------------------------------- Memory Backend & Controller -------------------------------------
 
-memory_controller = sst.Component("a64fx.memorycontroller", "memHierarchy.MemController")
+memory_controller = sst.Component("larc.memorycontroller", "memHierarchy.MemController")
 memory_controller.addParams({
-      "clock": A64FX_CLOCK,
-      "backend.access_time": A64FX_MEM_ACCESS,
-      "request_width": A64FX_MEMTOL2_PCMG_TPUT,
+      "clock": LARC_CLOCK,
+      "backend.access_time": LARC_MEM_ACCESS,
+      "request_width": LARC_MEMTOL2_PCMG_TPUT,
       "debug": DEBUG_MEM,
       "debug_level": DEBUG_LEVEL,
       "addr_range_start": memprops["start_addr"],
@@ -167,7 +167,7 @@ memory_controller.addParams({
 
 memory_backend = memory_controller.setSubComponent("backend", "memHierarchy.simpleMem")
 memory_backend.addParams({
-      "access_time": A64FX_MEM_ACCESS,
+      "access_time": LARC_MEM_ACCESS,
       "mem_size": memprops["size"],
       "request_width": 128,
 })
@@ -183,4 +183,5 @@ link_l1cache_l2cache = sst.Link("link_l1cache_l2cache_link")
 link_l1cache_l2cache.connect( (l1cache, "low_network_0", "50ps"), (l2cache, "high_network_0", "50ps") )
 link_mem_bus = sst.Link("link_mem_bus_link")
 link_mem_bus.connect( (l2cache, "low_network_0", "300ps"), (memory_controller, "direct_link", "300ps") )
+
 # ---------------------------------------------- Links ------------------------------------------------
