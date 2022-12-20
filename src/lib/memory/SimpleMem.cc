4812031d21dc9c170d2028428bccfdc26017ffca
Rahat <rahat.muneeb@hatless.studio>
2022-12-20 14:29:35 +0000

    Memory shared_ptr decl changes

--- src/lib/memory/SimpleMem.cc
+++ src/lib/memory/SimpleMem.cc
@@ -3,14 +3,14 @@
 namespace simeng {
 namespace memory {
 SimpleMem::SimpleMem(size_t size) {
-  memory_ = std::make_shared<char*>(new char[size]);
+  memory_ = std::make_shared<char[]>(new char[size]);
   memSize_ = size;
 }
 
 SimpleMem::~SimpleMem(){};
 
-std::shared_ptr<char*> SimpleMem::getMemory() {
-  return std::shared_ptr<char*>(memory_);
+std::shared_ptr<char[]> SimpleMem::getMemory() {
+  return std::shared_ptr<char[]>(memory_);
 };
 size_t SimpleMem::getMemorySize() { return memSize_; }
 
