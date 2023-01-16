e6583f7615a2b589f8d73661eaa45defe3fcd39b
Rahat Muneeb <rahat@pop-os.localdomain>
2023-01-16 19:24:15 +0000

    Added PageTable definition

---
+++ src/include/simeng/OS/PageTable.hh
@@ -0,0 +1,27 @@
+#include <map>
+#include <memory>
+namespace simeng {
+namespace kernel {
+
+struct PTEntry {
+  uint64_t vaddr;
+  uint64_t physAddr;
+  uint64_t size;
+};
+
+class PageTable {
+ private:
+  uint32_t pageSize_;
+  std::shared_ptr<std::map<uint64_t, PTEntry*>> table_;
+
+  bool allocatePTEntry(uint64_t addr);
+  uint64_t calculateOffset(uint64_t vaddr, uint64_t physfStartAddr);
+
+ public:
+  PageTable();
+  ~PageTable();
+  bool isAddrAllocated(uint64_t addr, uint64_t size);
+};
+
+}  // namespace kernel
+}  // namespace simeng
