39bcff6c5f4fed122f747b0c60a5c83b3a2ee6dc
Rahat Muneeb <rahat@pop-os.localdomain>
2023-01-17 09:30:49 +0000

    Added changes to elf.cc/hh

--- src/include/simeng/Elf.hh
+++ src/include/simeng/Elf.hh
@@ -19,6 +19,7 @@ struct ElfHeader {
   uint64_t physicalAddress;
   uint64_t fileSize;
   uint64_t memorySize;
+  char* headerData = nullptr;
 };
 
 /** A processed Executable and Linkable Format (ELF) file. */
@@ -29,12 +30,16 @@ class Elf {
   uint64_t getProcessImageSize() const;
   bool isValid() const;
   uint64_t getEntryPoint() const;
+  std::vector<ElfHeader*>& getProcessedHeaders();
+  uint64_t getMaxVirtAddr();
 
  private:
   uint64_t entryPoint_;
-  std::vector<ElfHeader> headers_;
+  std::vector<ElfHeader*> headers_;
   bool isValid_ = false;
-  uint64_t processImageSize_;
+  uint64_t processImageSize_ = 0;
+  std::vector<ElfHeader*> processedHeaders_;
+  uint64_t maxVirtAddr_ = 0;
 };
 
 }  // namespace simeng
