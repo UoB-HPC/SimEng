1ce6871a2d563b2b10d1b71e080a1e3622a92fc6
Rahat Muneeb <rahat@pop-os.localdomain>
2023-01-17 18:04:19 +0000

    Added PageTable functions

--- src/include/simeng/kernel/PageTable.hh
+++ src/include/simeng/kernel/PageTable.hh
@@ -4,23 +4,29 @@ namespace simeng {
 namespace kernel {
 
 struct PTEntry {
-  uint64_t vaddr;
-  uint64_t physAddr;
-  uint64_t size;
+  uint64_t baseVAddr;
+  uint64_t basePhyAddr;
 };
 
 class PageTable {
+  using TableItr = typename std::map<uint64_t, PTEntry*>::iterator;
+
  private:
-  uint32_t pageSize_;
+  const uint32_t pageSize_ = 4096;
   std::shared_ptr<std::map<uint64_t, PTEntry*>> table_;
 
-  bool allocatePTEntry(uint64_t addr);
-  uint64_t calculateOffset(uint64_t vaddr, uint64_t physfStartAddr);
+  uint64_t allocatePTEntry(uint64_t vaddr, uint64_t physAddr);
+  uint64_t deletePTEntry(uint64_t vaddr);
+  uint64_t calculateOffset(uint64_t vaddr);
+  TableItr find(uint64_t vaddr);
 
  public:
   PageTable();
   ~PageTable();
-  bool isAddrAllocated(uint64_t addr, uint64_t size);
+  bool createMapping(uint64_t vaddr, uint64_t basePhyAddr, size_t size);
+  bool deleteMapping(uint64_t vaddr, size_t size);
+  bool isMapped(uint64_t vaddr);
+  uint64_t translate(int64_t vaddr);
 };
 
 }  // namespace kernel
