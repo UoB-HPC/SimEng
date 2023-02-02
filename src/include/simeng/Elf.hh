69c685443ea59065f7748e7ad780d3260b9e1ff3
Rahat Muneeb <rahat@pop-os.localdomain>
2023-02-02 02:02:18 +0000

    Added comments to ELF.cc/hh and MMU.cc/hh

--- src/include/simeng/Elf.hh
+++ src/include/simeng/Elf.hh
@@ -27,18 +27,34 @@ class Elf {
  public:
   Elf(std::string path);
   ~Elf();
+
+  /** Method to return ELF process image size. */
   uint64_t getProcessImageSize() const;
+
+  /** Method to return validility of the ELF parsing process. */
   bool isValid() const;
+
+  /** Method which returns the entry point. */
   uint64_t getEntryPoint() const;
+
+  /** Method which returns all processed ELF Headers. */
   std::vector<ElfHeader*>& getProcessedHeaders();
+
+  /** Method which return maximum virtual address from the processed headers. */
   uint64_t getMaxVirtAddr();
 
  private:
+  /** Entry point of the ELF. */
   uint64_t entryPoint_;
+  /** Vector which holds all ELF headers. */
   std::vector<ElfHeader*> headers_;
+  /** Bool which holds if the ELF parsing was done correctly. */
   bool isValid_ = false;
+  /** Size of the ELF image. */
   uint64_t processImageSize_ = 0;
+  /** Vector which holds all processed ELF headers. */
   std::vector<ElfHeader*> processedHeaders_;
+  /** Max virtual address from ELF headers. */
   uint64_t maxVirtAddr_ = 0;
 };
 
