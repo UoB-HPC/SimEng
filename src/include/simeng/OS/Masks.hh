5bd325fb0b9f6b7393956a46476e7708e9b01152
Rahat Muneeb <rahat@pop-os.localdomain>
2023-02-13 11:01:55 +0000

    added conexpr to all masks declaration and remove bfd define from backward.cc

--- src/include/simeng/OS/Masks.hh
+++ src/include/simeng/OS/Masks.hh
@@ -14,19 +14,19 @@ namespace kernel {
 // https://stackoverflow.com/questions/6716946/why-do-x86-64-systems-have-only-a-48-bit-virtual-address-space#:~:text=They%20use%20an%20instruction%20set,be%20needed%20for%20many%20years.
 namespace masks {
 namespace get {
-static const uint64_t addr = 0x0FFFFFFFFFFFFFFF;
-static const uint64_t fault = 0xF000000000000000;
+static constexpr uint64_t addr = 0x0FFFFFFFFFFFFFFF;
+static constexpr uint64_t fault = 0xF000000000000000;
 }  // namespace get
 
 namespace faults {
 namespace pagetable {
-static const uint64_t nofault = 0x0;
-static const uint64_t fault = 0x8000000000000000;
-static const uint64_t ignored = 0x3000000000000000;
-static const uint64_t translate = 0x4000000000000000;
-static const uint64_t map = 0x2000000000000000;
-static const uint64_t unmap = 0x1000000000000000;
-static const uint64_t dataAbort = 0x5000000000000000;
+static constexpr uint64_t nofault = 0x0;
+static constexpr uint64_t fault = 0x8000000000000000;
+static constexpr uint64_t ignored = 0x3000000000000000;
+static constexpr uint64_t translate = 0x4000000000000000;
+static constexpr uint64_t map = 0x2000000000000000;
+static constexpr uint64_t unmap = 0x1000000000000000;
+static constexpr uint64_t dataAbort = 0x5000000000000000;
 }  // namespace pagetable
 
 static constexpr uint64_t getFaultCode(uint64_t value) {
