6efea685648e2bb81284c8b33531951b221e59f3
Rahat Muneeb <rahat@pop-os.localdomain>
2023-02-17 19:53:49 +0000

    Improved comments in Math.hh

--- src/include/simeng/util/Math.hh
+++ src/include/simeng/util/Math.hh
@@ -1,56 +1,58 @@
 #pragma once
 #include <iostream>
 
-/** This function checks if a number n is a power of 2. */
+/** This function checks if number 'n' is a power of 2. */
 template <class T>
 static constexpr bool isPow2(const T& n) {
-  // Given that n is non-zero, and a power of 2 it can be represented by x bits
-  // with only the most significant bit equal to 1 i.e 8 = 0x1000. Subtracting
-  // one from n gives a bit pattern with x-1 number of 1s i.e 0x1000 - 0x1 =
-  // 0x111. Using 'arithmetic &' on n and (n-1) will give 0 is n is power of 2
-  // because of the bit patterns mentioned above. for e.g 0x1000 - 0x111 = 0
+  //  Given that 'n' is non-zero, and a power of 2, it can be represented by x
+  //  bits with only the most significant bit equalling 1 e.g. 8 = 0x1000.
+  //  Subtracting one from 'n' gives us a bit of pattern with x-1 number of 1s
+  //  e.g. 0x1000 - 1 = 0x111. Using the arithmetic & operation between 'n' and
+  //  'n' - 1 will give 0 if 'n' is a power of 2 because there will be no two
+  //  ones in same the bit position (for example with the above patterns 0x1000
+  //  & 0x0111 = 0x0000). To ensure the edge case of 0 is handled, we then
+  //  logical && the logical negation of the result with 'n'. The logical
+  //  negotiation of the previous result will be 0 if non-zero and 1 otherwise.
+  //  Therefore, when
+  //  &&'ed with 'n', the return value is only true if the first result is 0
+  //  (representing a power of 2) and 'n' is non-zero.
   return n && !(n & (n - 1));
 }
 
-/**
- * This function is used to transform value such that it is equal to largest
- * multiple of align which is lower than value.
- */
+/** This function is used to round 'val' down to the nearest multiple of
+ * 'align'. 'align' must be a power of two.*/
 template <class T, class U>
 static constexpr T downAlign(const T& val, const U& align) {
   if (!isPow2(align)) {
-    std::cerr << "Alignment value is not power of 2" << std::endl;
+    std::cerr << "[SimEng::Math] Alignment value is not power of 2"
+              << std::endl;
     std::exit(1);
   }
-  // The value of align is a power of 2 and it can be represented by x bits
-  // with only the most significant bit equal to 1 i.e 8 = 0x1000. Subtracting
-  // one from align gives a bit pattern with x-1 number of 1s
-  // i.e 0x1000 - 0x1 = 0x111. Using the "artihmetic &" operation on value and
-  // bit reversed mask will convert all x-1 least significant bits of value to
-  // 0, hence down aligning value. e.g
-  // 56 (0x00111000) down aligned by 16 (0x10000) is 48 (00x00110000)
-  // => mask = 16 - 1 => mask = 0x00010000 - 0x1 = 0x00001111
-  // => 0x00111000 & ~(0x00001111) => 0x00111000 & 0x11110000 => 0x00110000
+  // Given that the value of align must be a power of 2, it can be represented
+  // by x bits with only the most significant bit equalling 1 e.g. 8 = 0x1000.
+  // Subtracting one from align gives us a bit of pattern with x-1 number of 1s
+  // e.g. 0x1000 - 1 = 0x111. This operation forms our mask for the next step.
+  // Using the arithmetic & operation between 'val' and our mask bit reversed,
+  // we convert val to be the closest multiple of align less than it. Hence we
+  // have "down aligned" val. As an example with T,U = uint8_t, val = 61
+  // (0x00111101) align = 16 (0x00010000) mask = 0x00001111 ~mask = 0x1111000
+  // val & ~mask = 0x00110000 (48)
   T mask = (T)align - 1;
   return val & ~mask;
 }
 
-/**
- * This function is used to transform value such that it is equal to the
- * smallest multiple of align which is greater than value.
- */
+/** This function is used to round 'val' up to the nearest multiple to align.
+ * 'align' must be a power of 2. */
 template <class T, class U>
 static constexpr T upAlign(const T& val, const U& align) {
   if (!isPow2(align)) {
     std::cerr << "Alignment value is not power of 2" << std::endl;
     std::exit(1);
   }
-  // The operation to upalign is conceptually similar to down align.
-  // However to upalign value we actually down align:
-  // y = (value + (align - 1)).
-  // y will always be a number than is greater than 1st multiple of
-  // 'align' larger than 'value' but less than the 2nd multiple of 'align'
-  // larger than 'value'.
+  // The operation of upAlign is very similar to the downAlign function however,
+  // we first add the derived mask value to val before "down aligning" it. This
+  // ensures that when we "down align" this new value, we get the next multiple
+  // of align that is greater than val not less than.
   T mask = (T)align - 1;
   return (val + mask) & ~mask;
 }
