a5e0b343f547005482cfa3ac7341086d7151bf64
Finn Wilkinson <finnwilkinson5@gmail.com>
2023-04-05 18:14:04 +0100

    Adapted test suite to work with new single MMU class.

--- test/unit/pipeline/FetchUnitTest.cc
+++ test/unit/pipeline/FetchUnitTest.cc
@@ -1,11 +1,12 @@
 #include "../MockArchitecture.hh"
 #include "../MockBranchPredictor.hh"
 #include "../MockInstruction.hh"
-#include "../MockMemoryInterface.hh"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "simeng/Instruction.hh"
 #include "simeng/arch/Architecture.hh"
+#include "simeng/memory/MMU.hh"
+#include "simeng/memory/SimpleMem.hh"
 #include "simeng/pipeline/FetchUnit.hh"
 #include "simeng/pipeline/PipelineBuffer.hh"
 
@@ -24,7 +25,9 @@ class PipelineFetchUnitTest : public testing::Test {
       : output(1, {}),
         fetchBuffer({{0, 16}, 0, 0}),
         completedReads(&fetchBuffer, 1),
-        fetchUnit(output, memory, 16, isa, predictor),
+        memory(std::make_shared<memory::SimpleMem>(1024)),
+        mmu(std::make_shared<memory::MMU>(memory, latency, fn, tid)),
+        fetchUnit(output, mmu, 16, isa, predictor),
         uop(new MockInstruction),
         uopPtr(uop) {
     uopPtr->setInstructionAddress(0);
@@ -34,12 +37,19 @@ class PipelineFetchUnitTest : public testing::Test {
 
  protected:
   PipelineBuffer<MacroOp> output;
-  MockMemoryInterface memory;
   MockArchitecture isa;
   MockBranchPredictor predictor;
 
-  MemoryReadResult fetchBuffer;
-  span<MemoryReadResult> completedReads;
+  memory::MemoryReadResult fetchBuffer;
+  span<memory::MemoryReadResult> completedReads;
+
+  const uint64_t latency = 0;
+  const uint64_t tid = 0;
+  VAddrTranslator fn = [](uint64_t vaddr, uint64_t pid) -> uint64_t {
+    return vaddr;
+  };
+  std::shared_ptr<memory::SimpleMem> memory;
+  std::shared_ptr<memory::MMU> mmu;
 
   FetchUnit fetchUnit;
 
@@ -52,13 +62,14 @@ class PipelineFetchUnitTest : public testing::Test {
 TEST_F(PipelineFetchUnitTest, Tick) {
   MacroOp macroOp = {uopPtr};
 
-  ON_CALL(memory, getCompletedReads()).WillByDefault(Return(completedReads));
-
   ON_CALL(isa, getMaxInstructionSize()).WillByDefault(Return(4));
 
   // Set the output parameter to a 1-wide macro-op
   EXPECT_CALL(isa, predecode(_, _, 0, _))
       .WillOnce(DoAll(SetArgReferee<3>(macroOp), Return(4)));
+
+  // Prefetch instructions from memory
+  mmu->requestInstrRead({0, 16});
 
   fetchUnit.tick();
 
@@ -88,22 +99,29 @@ TEST_F(PipelineFetchUnitTest, TickStalled) {
 TEST_F(PipelineFetchUnitTest, FetchUnaligned) {
   MacroOp macroOp = {uopPtr};
   ON_CALL(isa, getMaxInstructionSize()).WillByDefault(Return(4));
-  ON_CALL(memory, getCompletedReads()).WillByDefault(Return(completedReads));
+  mmu->clearCompletedIntrReads();
 
   // Set PC to 14, so there will not be enough data to start decoding
   EXPECT_CALL(isa, predecode(_, _, _, _)).Times(0);
+  fetchUnit.setProgramLength(1024);
   fetchUnit.updatePC(14);
+  fetchUnit.requestFromPC();
   fetchUnit.tick();
+  // Fetch ocurred on block 0->16, with bytes 14-16 being buffered. Hence, no
+  // decode
+  EXPECT_EQ(mmu->getCompletedInstrReads().size(), 1);
+  EXPECT_EQ(mmu->getCompletedInstrReads()[0].target.address, 0);
+  EXPECT_EQ(mmu->getCompletedInstrReads()[0].data.size(), 16);
 
   // Expect a block starting at address 16 to be requested when we fetch again
-  EXPECT_CALL(memory, requestRead(Field(&MemoryAccessTarget::address, 16), _))
-      .Times(1);
   fetchUnit.requestFromPC();
+  // Ensure that a block starting at address 16, of size 16-bytes, was fetched
+  EXPECT_EQ(mmu->getCompletedInstrReads()[1].target.address, 16);
+  EXPECT_EQ(mmu->getCompletedInstrReads()[1].data.size(), 16);
 
   // Tick again, expecting that decoding will now resume
-  MemoryReadResult nextBlockValue = {{16, 16}, 0, 1};
-  span<MemoryReadResult> nextBlock = {&nextBlockValue, 1};
-  EXPECT_CALL(memory, getCompletedReads()).WillOnce(Return(nextBlock));
+  memory::MemoryReadResult nextBlockValue = {{16, 16}, 0, 1};
+  span<memory::MemoryReadResult> nextBlock = {&nextBlockValue, 1};
   EXPECT_CALL(isa, predecode(_, _, _, _))
       .WillOnce(DoAll(SetArgReferee<3>(macroOp), Return(4)));
   fetchUnit.tick();
