e0bd27e1347e1da6e3cd2655d7da2f6170266750
Finn Wilkinson <56131608+FinnWilkinson@users.noreply.github.com>
2023-11-02 15:16:44 +0000

    SME SVCR logic fix (#339)
    This PR for the most part addresses issue #329 - correcting the SVCR and Streaming Mode logic introduced with SME. This is summaried as:
    
     - If a program is in Streaming Mode and calls SMSTART again, ZA and Vector registers are not zeroed out.
     - SVE Vector registers (and their aliases such as NEON and Scalar registers) are only zeroed out when SVCR.SM bit changes
     - SME's ZA register is only zeroed out when SVCR.ZA bit changes
     - SVCR can now be correctly updated using a regulat msr svcr, xt instruction in addition to smstart/smstop/msr svcr, #imm
     - More tests surrounding the SVCR logic have been added
    
    Additional to this, logic for the NEON muti-struct store with post index instructions introduced in PR #335 has been corrected via a small change to the metadata to correctly identify if a #imm or xt is used as said post index.

--- test/regression/aarch64/Exception.cc
+++ test/regression/aarch64/Exception.cc
@@ -51,6 +51,49 @@ TEST_P(Exception, SME_context_modes) {
   EXPECT_EQ(stdout_.substr(0, sizeof(err2) - 1), err2);
 }
 
+// Ensure that calling smstart/smstop such that the values in SVCR.SMZA do not
+// change doesn't cause a flush of the associated register files
+TEST_P(Exception, Null_Smstart_smstop_calls) {
+  RUN_AARCH64(R"(
+    smstart
+    dup z0.d, #3
+    smstart
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({3}, SVL / 8));
+
+  RUN_AARCH64(R"(
+    smstart
+    dup z0.d, #4
+    smstart sm
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({4}, SVL / 8));
+
+  RUN_AARCH64(R"(
+    smstart
+    dup z0.d, #5
+    smstart za
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({5}, SVL / 8));
+
+  RUN_AARCH64(R"(
+    dup z0.d, #6
+    smstop
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({6}, VL / 8));
+
+  RUN_AARCH64(R"(
+    dup z0.d, #7
+    smstop sm
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({7}, VL / 8));
+
+  RUN_AARCH64(R"(
+    dup z0.d, #8
+    smstop za
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({8}, VL / 8));
+}
+
 TEST_P(Exception, svcr) {
   // Check that smstart and smstop correctly change value of SVCR system
   // register, verified by the correctly performed behaviour
@@ -77,12 +120,27 @@ TEST_P(Exception, svcr) {
   CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({0}, VL / 8));
 
   RUN_AARCH64(R"(
+    # Ensure z regs get enabled when SM enabled
+    smstart sm
+    dup z0.d, #3
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({3}, SVL / 8));
+
+  RUN_AARCH64(R"(
     # Ensure z regs get zeroed out when SM disabled
     smstart
     dup z0.d, #3
     smstop
   )");
   CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({0}, VL / 8));
+
+  RUN_AARCH64(R"(
+    # Ensure z regs do not get zeroed out when ZA is disabled
+    smstart
+    dup z0.d, #3
+    smstop za
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({3}, SVL / 8));
 
   RUN_AARCH64(R"(
     # Ensure za reg gets zeroed out when ZA enabled
@@ -94,6 +152,79 @@ TEST_P(Exception, svcr) {
     fmopa za0.s, p0/m, p1/m, z0.s, z1.s
     smstop
     smstart
+  )");
+  for (int i = 0; i < (SVL / 8); i++) {
+    CHECK_MAT_ROW(ARM64_REG_ZA, i, uint32_t, fillNeon<uint32_t>({0}, SVL / 8));
+  }
+
+  // Check that changes to SVCR using msr svcr, xn work correctly
+  RUN_AARCH64(R"(
+    mov x4, #3
+    mov x5, #0
+    # Ensure vector length changes from SVE's to SME's
+    cntb x0
+    msr svcr, x4
+    cntb x1
+    msr svcr, x5
+    cntb x2
+  )");
+  EXPECT_EQ(getGeneralRegister<uint64_t>(0), VL / 8);
+  EXPECT_EQ(getGeneralRegister<uint64_t>(1), SVL / 8);
+  EXPECT_EQ(getGeneralRegister<uint64_t>(2), VL / 8);
+  EXPECT_EQ(getGeneralRegister<uint64_t>(0), getGeneralRegister<uint64_t>(2));
+  EXPECT_GT(getGeneralRegister<uint64_t>(1), getGeneralRegister<uint64_t>(0));
+  EXPECT_GT(SVL, VL);
+
+  RUN_AARCH64(R"(
+    mov x4, #3
+    # Ensure z regs get zeroed out when SM enabled
+    dup z0.d, #3
+    msr svcr, x4
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({0}, VL / 8));
+
+  RUN_AARCH64(R"(
+    mov x4, #1
+    # Ensure z regs get enabled when SM enabled
+    msr svcr, x4
+    dup z0.d, #3
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({3}, SVL / 8));
+
+  RUN_AARCH64(R"(
+    mov x4, #3
+    mov x5, #0
+    # Ensure z regs get zeroed out when SM disabled
+    msr svcr, x4
+    dup z0.d, #3
+    msr svcr, x5
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({0}, VL / 8));
+
+  RUN_AARCH64(R"(
+    # enable SM and ZA
+    mov x4, #3
+    # just disable ZA
+    mov x5, #1
+    # Ensure z regs do not get zeroed out when ZA is disabled
+    msr svcr, x4
+    dup z0.d, #3
+    msr svcr, x5
+  )");
+  CHECK_NEON(0, uint64_t, fillNeon<uint64_t>({3}, SVL / 8));
+
+  RUN_AARCH64(R"(
+    mov x4, #3
+    mov x5, #0
+    # Ensure za reg gets zeroed out when ZA enabled
+    msr svcr, x4
+    dup z0.s, #2
+    dup z1.s, #3
+    ptrue p0.s
+    ptrue p1.s
+    fmopa za0.s, p0/m, p1/m, z0.s, z1.s
+    msr svcr, x5
+    msr svcr, x4
   )");
   for (int i = 0; i < (SVL / 8); i++) {
     CHECK_MAT_ROW(ARM64_REG_ZA, i, uint32_t, fillNeon<uint32_t>({0}, SVL / 8));
