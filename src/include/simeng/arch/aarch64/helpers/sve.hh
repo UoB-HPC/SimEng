c9540435c3177d85609e168e6a31b9417d0d6dd9
Finn Wilkinson <finnwilkinson5@gmail.com>
2024-01-18 18:13:39 +0000

    Jenkins fabsf fix.

--- src/include/simeng/arch/aarch64/helpers/sve.hh
+++ src/include/simeng/arch/aarch64/helpers/sve.hh
@@ -751,26 +751,25 @@ sveFrintnPredicated(std::vector<RegisterValue>& operands,
   const uint64_t* p = operands[1].getAsVector<uint64_t>();
   const T* n = operands[2].getAsVector<T>();
 
-  bool isFloat = (sizeof(T) == sizeof(float));
   const uint16_t partition_num = VL_bits / (sizeof(T) * 8);
   T out[256 / sizeof(T)] = {0};
 
   for (int i = 0; i < partition_num; i++) {
     uint64_t shifted_active = 1ull << ((i % (64 / sizeof(T))) * sizeof(T));
     if (p[i / (64 / sizeof(T))] & shifted_active) {
-      // Get remainder
-      T trunc = isFloat ? std::truncf(n[i]) : std::trunc(n[i]);
-      T rem = isFloat ? (std::fabsf(n[i] - trunc)) : (std::fabs(n[i] - trunc));
-      // On tie, round to even
-      if (rem == static_cast<T>(0.5)) {
+      // Get truncation
+      T trunc = std::trunc(n[i]);
+      // On tie, round to nearest even
+      if (std::fabs(n[i] - trunc) == static_cast<T>(0.5)) {
         T addand = (trunc > static_cast<T>(0.0)) ? static_cast<T>(1)
                                                  : static_cast<T>(-1);
-        bool isEven = isFloat ? (std::fmodf(trunc, 2.0f) == 0.0f)
-                              : (std::fmod(trunc, 2.0) == 0.0);
-        out[i] = isEven ? trunc : (trunc + addand);
+        // If odd, add the addand
+        out[i] = (std::fmod(trunc, static_cast<T>(2.0)) == static_cast<T>(0.0))
+                     ? trunc
+                     : (trunc + addand);
       } else {
         // Else, round to nearest
-        out[i] = isFloat ? std::roundf(n[i]) : std::round(n[i]);
+        out[i] = std::round(n[i]);
       }
     } else {
       out[i] = d[i];
