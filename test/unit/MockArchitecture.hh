21b06de9819c9032e129022fa0d48db9ca2ab268
dANW34V3R <dan.weaver@hotmail.co.uk>
2024-02-06 22:09:43 +0000

    Update invalid instruction bits as now decodes as C.NOP

--- test/unit/MockArchitecture.hh
+++ test/unit/MockArchitecture.hh
@@ -9,9 +9,8 @@ namespace simeng {
 class MockArchitecture : public arch::Architecture {
  public:
   MOCK_CONST_METHOD4(predecode,
-                     uint8_t(const void* ptr, uint8_t bytesAvailable,
-                             uint64_t instructionAddress, MacroOp& output))
-  int8_tconst void* uint8_tuint64_tMacroOp&;
+                     int8_t(const void* ptr, uint8_t bytesAvailable,
+                            uint64_t instructionAddress, MacroOp& output));
   MOCK_CONST_METHOD1(canRename, bool(Register reg));
   MOCK_CONST_METHOD1(getSystemRegisterTag, int32_t(uint16_t reg));
   MOCK_CONST_METHOD3(handleException,
