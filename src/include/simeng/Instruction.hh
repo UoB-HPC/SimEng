61b8a82dbac3d51d6a11d35d23b2b3aec8d4aa5e
Finn Wilkinson <finnwilkinson5@gmail.com>
2024-02-09 11:37:13 +0000

    Removed instruction.cc of base class and re-structured instruction.hh.

--- src/include/simeng/Instruction.hh
+++ src/include/simeng/Instruction.hh
@@ -32,9 +32,7 @@ class Instruction {
  public:
   virtual ~Instruction(){};
 
-  /** Check whether an exception has been encountered while processing this
-   * instruction. */
-  bool exceptionEncountered() const;
+  // ------ Abstract Functions ------
 
   /** Retrieve the source registers this instruction reads. */
   virtual const span<Register> getSourceRegisters() const = 0;
@@ -62,24 +60,6 @@ class Instruction {
   /** Check whether the operand at index `i` has had a value supplied. */
   virtual bool isOperandReady(int i) const = 0;
 
-  /** Check whether all operand values have been supplied, and the instruction
-   * is ready to execute. */
-  virtual bool canExecute() const = 0;
-
-  /** Execute the instruction. */
-  virtual void execute() = 0;
-
-  /** Check whether the instruction has executed and has results ready to
-   * write back. */
-  bool hasExecuted() const;
-
-  /** Mark the instruction as ready to commit. */
-  void setCommitReady();
-
-  /** Check whether the instruction has written its values back and is ready to
-   * commit. */
-  bool canCommit() const;
-
   /** Retrieve register results. */
   virtual const span<RegisterValue> getResults() const = 0;
 
@@ -96,23 +76,11 @@ class Instruction {
   /** Retrieve supplied memory data. */
   virtual span<const RegisterValue> getData() const = 0;
 
-  /** Check whether all required data has been supplied. */
-  bool hasAllData() const;
-
   /** Early misprediction check; see if it's possible to determine whether the
    * next instruction address was mispredicted without executing the
    * instruction. Returns a {mispredicted, target} tuple representing whether
    * the instruction was mispredicted, and the correct target address. */
   virtual std::tuple<bool, uint64_t> checkEarlyBranchMisprediction() const = 0;
-
-  /** Check for misprediction. */
-  bool wasBranchMispredicted() const;
-
-  /** Retrieve branch address. */
-  uint64_t getBranchAddress() const;
-
-  /** Was the branch taken? */
-  bool wasBranchTaken() const;
 
   /** Retrieve branch type. */
   virtual BranchType getBranchType() const = 0;
@@ -134,77 +102,145 @@ class Instruction {
   /** Is this a branch operation? */
   virtual bool isBranch() const = 0;
 
-  /** Set this instruction's instruction memory address. */
-  void setInstructionAddress(uint64_t address);
-
-  /** Get this instruction's instruction memory address. */
-  uint64_t getInstructionAddress() const;
-
-  /** Supply a branch prediction. */
-  void setBranchPrediction(BranchPrediction prediction);
-
-  /** Get a branch prediction. */
-  BranchPrediction getBranchPrediction() const;
-
-  /** Set this instruction's sequence ID. */
-  void setSequenceId(uint64_t seqId);
-
-  /** Retrieve this instruction's sequence ID. */
-  uint64_t getSequenceId() const;
-
-  /** Set this instruction's instruction ID. */
-  void setInstructionId(uint64_t insnId);
-
-  /** Retrieve this instruction's instruction ID. */
-  uint64_t getInstructionId() const;
-
-  /** Mark this instruction as flushed. */
-  void setFlushed();
-
-  /** Check whether this instruction has been flushed. */
-  bool isFlushed() const;
-
   /** Retrieve the instruction group this instruction belongs to. */
   virtual uint16_t getGroup() const = 0;
 
-  /** Retrieve the number of cycles this instruction will take to execute. */
-  uint16_t getLatency() const;
+  /** Check whether all operand values have been supplied, and the instruction
+   * is ready to execute. */
+  virtual bool canExecute() const = 0;
 
-  /** Retrieve the number of cycles this instruction will take to be prcoessed
-   * by the LSQ. */
-  uint16_t getLSQLatency() const;
-
-  /** Retrieve the number of cycles this instruction will block the unit
-   * executing it. */
-  uint16_t getStallCycles() const;
+  /** Execute the instruction. */
+  virtual void execute() = 0;
 
   /** Get this instruction's supported set of ports. */
   virtual const std::vector<uint16_t>& getSupportedPorts() = 0;
 
-  /** Is this a micro-operation? */
-  bool isMicroOp() const;
+  // ------ Defined Functions ------
 
-  /** Is this the last uop in the possible sequence of decoded uops? */
-  bool isLastMicroOp() const;
+  /** Set this instruction's sequence ID. */
+  void setSequenceId(uint64_t seqId) { sequenceId_ = seqId; }
+
+  /** Retrieve this instruction's sequence ID. */
+  uint64_t getSequenceId() const { return sequenceId_; }
+
+  /** Set this instruction's instruction ID. */
+  void setInstructionId(uint64_t insnId) { instructionId_ = insnId; }
+
+  /** Retrieve this instruction's instruction ID. */
+  uint64_t getInstructionId() const { return instructionId_; }
+
+  /** Set this instruction's instruction memory address. */
+  void setInstructionAddress(uint64_t address) {
+    instructionAddress_ = address;
+  }
+
+  /** Get this instruction's instruction memory address. */
+  uint64_t getInstructionAddress() const { return instructionAddress_; }
+
+  /** Supply a branch prediction. */
+  void setBranchPrediction(BranchPrediction prediction) {
+    prediction_ = prediction;
+  }
+
+  /** Get a branch prediction. */
+  BranchPrediction getBranchPrediction() const { return prediction_; }
+
+  /** Retrieve branch address. */
+  uint64_t getBranchAddress() const { return branchAddress_; }
+
+  /** Was the branch taken? */
+  bool wasBranchTaken() const { return branchTaken_; }
+
+  /** Check for misprediction. */
+  bool wasBranchMispredicted() const {
+    assert(executed_ &&
+           "Branch misprediction check requires instruction to have executed");
+    // Flag as mispredicted if taken state was wrongly predicted, or taken and
+    // predicted target is wrong
+    return (branchTaken_ != prediction_.taken ||
+            (prediction_.target != branchAddress_));
+  }
+
+  /** Check whether an exception has been encountered while processing this
+   * instruction. */
+  bool exceptionEncountered() const { return exceptionEncountered_; }
+
+  /** Check whether all required data has been supplied. */
+  bool hasAllData() const { return (dataPending_ == 0); }
+
+  /** Check whether the instruction has executed and has results ready to
+   * write back. */
+  bool hasExecuted() const { return executed_; }
+
+  /** Retrieve the number of cycles this instruction will take to execute. */
+  uint16_t getLatency() const { return latency_; }
+
+  /** Retrieve the number of cycles this instruction will block the unit
+   * executing it. */
+  uint16_t getStallCycles() const { return stallCycles_; }
+
+  /** Retrieve the number of cycles this instruction will take to be prcoessed
+   * by the LSQ. */
+  uint16_t getLSQLatency() const { return lsqExecutionLatency_; }
 
   /** Set the micro-operation in an awaiting commit signal state. */
-  void setWaitingCommit();
+  void setWaitingCommit() { waitingCommit_ = true; }
 
   /** Is the micro-operation in an awaiting commit state? */
-  bool isWaitingCommit() const;
+  bool isWaitingCommit() const { return waitingCommit_; }
+
+  /** Mark the instruction as ready to commit. */
+  void setCommitReady() { canCommit_ = true; }
+
+  /** Check whether the instruction has written its values back and is ready to
+   * commit. */
+  bool canCommit() const { return canCommit_; }
+
+  /** Mark this instruction as flushed. */
+  void setFlushed() { flushed_ = true; }
+
+  /** Check whether this instruction has been flushed. */
+  bool isFlushed() const { return flushed_; }
+
+  /** Is this a micro-operation? */
+  bool isMicroOp() const { return isMicroOp_; }
+
+  /** Is this the last uop in the possible sequence of decoded uops? */
+  bool isLastMicroOp() const { return isLastMicroOp_; }
 
   /** Get arbitrary micro-operation index. */
-  int getMicroOpIndex() const;
+  int getMicroOpIndex() const { return microOpIndex_; }
 
  protected:
-  /** Whether an exception has been encountered. */
-  bool exceptionEncountered_ = false;
+  // Instruction Info
+  /** This instruction's instruction ID used to group micro-operations together
+   * by macro-op; a higher ID represents a chronologically newer instruction. */
+  uint64_t instructionId_ = 0;
+
+  /** This instruction's sequence ID; a higher ID represents a chronologically
+   * newer instruction. */
+  uint64_t sequenceId_ = 0;
 
   /** The location in memory of this instruction was decoded at. */
-  uint64_t instructionAddress_;
+  uint64_t instructionAddress_ = 0;
 
+  // Execution
   /** Whether or not this instruction has been executed. */
   bool executed_ = false;
+
+  /** The number of cycles this instruction takes to execute. */
+  uint16_t latency_ = 1;
+
+  /** The number of cycles a load or store instruction takes to execute within
+   * the load/store queue. */
+  uint16_t lsqExecutionLatency_ = 1;
+
+  /** The number of cycles this instruction will stall the unit executing it
+   * for. */
+  uint16_t stallCycles_ = 1;
+
+  /** The execution ports that this instruction can be issued to. */
+  std::vector<uint16_t> supportedPorts_ = {};
 
   /** Whether or not this instruction is ready to commit. */
   bool canCommit_ = false;
@@ -231,26 +267,11 @@ class Instruction {
   int64_t knownOffset_ = 0;
 
   // Flushing
-  /** This instruction's sequence ID; a higher ID represents a chronologically
-   * newer instruction. */
-  uint64_t sequenceId_;
-
   /** Has this instruction been flushed? */
   bool flushed_ = false;
 
-  /** The number of cycles this instruction takes to execute. */
-  uint16_t latency_ = 1;
-
-  /** The number of cycles a load or store instruction takes to execute within
-   * the load/store queue. */
-  uint16_t lsqExecutionLatency_ = 1;
-
-  /** The number of cycles this instruction will stall the unit executing it
-   * for. */
-  uint16_t stallCycles_ = 1;
-
-  /** The execution ports that this instruction can be issued to. */
-  std::vector<uint16_t> supportedPorts_ = {};
+  /** Whether an exception has been encountered. */
+  bool exceptionEncountered_ = false;
 
   // Micro operations
   /** Is a resultant micro-operation from an instruction split? */
@@ -259,10 +280,6 @@ class Instruction {
   /** Whether or not this instruction is the last uop in the possible sequence
    * of decoded uops. Default case is that it is. */
   bool isLastMicroOp_ = true;
-
-  /** This instruction's instruction ID used to group micro-operations together
-   * by macro-op; a higher ID represents a chronologically newer instruction. */
-  uint64_t instructionId_;
 
   /** Is the micro-operation in a committable state but must wait for all
    * associated micro-operations to also be committable? */
