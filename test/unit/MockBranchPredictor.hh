3c377b33b8b7d117dc3c8f7eb4fa5fa01fef2ed9
Alex Cockrean <84676155+ABenC377@users.noreply.github.com>
2024-06-05 09:09:20 +0100

    rebase

--- test/unit/MockBranchPredictor.hh
+++ test/unit/MockBranchPredictor.hh
@@ -8,9 +8,8 @@ namespace simeng {
 /** Mock implementation of the `BranchPredictor` interface. */
 class MockBranchPredictor : public BranchPredictor {
  public:
-  MOCK_METHOD4(predict,
-               BranchPrediction(uint64_t address, BranchType type,
-                                int64_t knownTarget, bool getPrediction));
+  MOCK_METHOD3(predict, BranchPrediction(uint64_t address, BranchType type,
+                                         int64_t knownTarget));
   MOCK_METHOD5(update,
                void(uint64_t address, bool taken, uint64_t targetAddress,
                     BranchType type, uint64_t instructionId));
