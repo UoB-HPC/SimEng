28a750659adba8d0ec9169b5cbadd2eca67c110a
Alex Cockrean <84676155+ABenC377@users.noreply.github.com>
2024-06-05 10:18:11 +0100

    rebase

--- src/include/simeng/branchpredictors/AlwaysNotTakenPredictor.hh
+++ src/include/simeng/branchpredictors/AlwaysNotTakenPredictor.hh
@@ -11,8 +11,7 @@ class AlwaysNotTakenPredictor : public BranchPredictor {
   /** Generate a branch prediction for the specified instruction address; will
    * always predict not taken. */
   BranchPrediction predict(uint64_t address, BranchType type,
-                                          int64_t knownOffset,
-                                          bool isLoop = false) override;
+                           int64_t knownOffset, bool isLoop = false) override;
 
   /** Updates appropriate predictor model objects based on the address, type and
    * outcome of the branch instruction.  Update must be called on
