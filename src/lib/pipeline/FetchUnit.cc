052c4d1a831073b27d65919b89b62e5fcf65dbd2
Alex Cockrean <84676155+ABenC377@users.noreply.github.com>
2024-07-25 14:50:42 +0100

    Adding comments to explain bug fix in FetchUnit.cc

--- src/lib/pipeline/FetchUnit.cc
+++ src/lib/pipeline/FetchUnit.cc
@@ -177,7 +177,7 @@ void FetchUnit::tick() {
     } else if (loopBufferState_ == LoopBufferState::WAITING &&
                pc_ == loopBoundaryAddress_) {
       // Now that the loopBoundaryAddress_ has been set, start filling Loop
-      // Buffer is the branch predictor tells us to reenter the detected loop
+      // Buffer if the branch predictor tells us to reenter the detected loop
       if (macroOp[0]->isBranch() && !macroOp[0]->getBranchPrediction().taken) {
         // If branch is not taken then detected loop is no longer relevant,
         // therefore Loop Buffer stays idle
