a423a3eeacc48ae379056dc3b5fa2c2ba671cd64
Finn Wilkinson <finnwilkinson5@gmail.com>
2024-08-14 12:03:45 +0100

    Added SME instructions USMOPA and USMOPS (S and D) support and regression tests.

--- src/lib/arch/aarch64/InstructionMetadata.cc
+++ src/lib/arch/aarch64/InstructionMetadata.cc
@@ -1683,6 +1683,14 @@ InstructionMetadata::InstructionMetadata(const cs_insn& insn)
       operands[0].access = CS_AC_READ;
       operands[1].access = CS_AC_READ;
       break;
+    case Opcode::AArch64_USMOPA_MPPZZ_D:
+      [[fallthrough]];
+    case Opcode::AArch64_USMOPA_MPPZZ_S:
+      [[fallthrough]];
+    case Opcode::AArch64_USMOPS_MPPZZ_D:
+      [[fallthrough]];
+    case Opcode::AArch64_USMOPS_MPPZZ_S:
+      [[fallthrough]];
     case Opcode::AArch64_SUMOPA_MPPZZ_D:
       [[fallthrough]];
     case Opcode::AArch64_SUMOPA_MPPZZ_S:
