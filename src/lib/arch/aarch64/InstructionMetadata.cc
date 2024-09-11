3d7913a85198b72f0ab30a14fd36450421f925c1
Joseph Moore <moore.joseph002@gmail.com>
2024-09-11 21:49:46 +0100

    Added tests for ftsmul and fixed some broken logic

--- src/lib/arch/aarch64/InstructionMetadata.cc
+++ src/lib/arch/aarch64/InstructionMetadata.cc
@@ -448,6 +448,10 @@ InstructionMetadata::InstructionMetadata(const cs_insn& insn)
       [[fallthrough]];
     case Opcode::AArch64_FMUL_ZZZ_S:
       [[fallthrough]];
+    case Opcode::AArch64_FTSMUL_ZZZ_D:
+      [[fallthrough]];
+    case Opcode::AArch64_FTSMUL_ZZZ_S:
+      [[fallthrough]];
     case Opcode::AArch64_SMAX_ZI_D:
       [[fallthrough]];
     case Opcode::AArch64_SMAX_ZI_S:
