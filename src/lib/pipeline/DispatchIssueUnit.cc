0889f12ff707c7b0edf93f8203c2c638b7647484
Finn Wilkinson <finnwilkinson5@gmail.com>
2024-11-07 13:59:03 +0000

    Updated DI unit port allocation logic and relevant unit tests.

--- src/lib/pipeline/DispatchIssueUnit.cc
+++ src/lib/pipeline/DispatchIssueUnit.cc
@@ -75,38 +75,33 @@ void DispatchIssueUnit::tick() {
       continue;
     }
 
-    // Try find an available RS
-    uint16_t port = 0;
-    uint16_t RS_Index = 0;
-    uint16_t RS_Port = 0;
-    ReservationStation* rs;
-    bool foundRS = false;
-    while (false == foundRS && supportedPorts.size() > 0) {
-      // Allocate issue port to uop
-      port = portAllocator_.allocate(supportedPorts);
-      RS_Index = portMapping_[port].first;
-      RS_Port = portMapping_[port].second;
-      assert(RS_Index < reservationStations_.size() &&
-             "Allocated port inaccessible");
-      rs = &reservationStations_[RS_Index];
-      // When appropriate, stall uop or input buffer if stall buffer full
+    // Loop through all ports and remove any who's RS is at capacity or dispatch
+    // rate has been met
+    auto portIt = supportedPorts.begin();
+    while (portIt != supportedPorts.end()) {
+      uint16_t RS_Index = portMapping_[*portIt].first;
+      ReservationStation* rs = &reservationStations_[RS_Index];
       if (rs->currentSize == rs->capacity ||
           dispatches_[RS_Index] == rs->dispatchRate) {
-        // Deallocate port given
-        portAllocator_.deallocate(port);
-        supportedPorts.erase(
-            std::find(supportedPorts.begin(), supportedPorts.end(), port));
+        portIt = supportedPorts.erase(portIt);
       } else {
-        foundRS = true;
+        portIt++;
       }
     }
-    // If no port with capacity or available dispatch rate found. Stall and
-    // return.
-    if (false == foundRS) {
+    // If no ports left, stall and return
+    if (supportedPorts.size() == 0) {
       input_.stall(true);
       rsStalls_++;
       return;
     }
+
+    // Find an available RS
+    uint16_t port = portAllocator_.allocate(supportedPorts);
+    uint16_t RS_Index = portMapping_[port].first;
+    uint16_t RS_Port = portMapping_[port].second;
+    assert(RS_Index < reservationStations_.size() &&
+           "Allocated port inaccessible");
+    ReservationStation* rs = &reservationStations_[RS_Index];
 
     // Assume the uop will be ready
     bool ready = true;
