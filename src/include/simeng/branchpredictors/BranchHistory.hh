9afa83c600c45b6281e59e31688cf02c1fa8b0bf
Alex Cockrean <84676155+ABenC377@users.noreply.github.com>
2024-12-09 12:46:02 +0000

    Cleaning up BranchHistory.hh comments

--- src/include/simeng/branchpredictors/BranchHistory.hh
+++ src/include/simeng/branchpredictors/BranchHistory.hh
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <iostream>
-
 namespace simeng {
 /** A class for storing a branch history.  Needed for cases where a branch
  * history of more than 64 bits is required.  This class makes it easier to
@@ -9,7 +7,7 @@ namespace simeng {
  * sophisticated branch predictors.
  *
  * The bits of the branch history are stored in a vector of uint64_t values,
- * and their access/manipulation is facilitated by the public functions.*/
+ * and their access/manipulation is facilitated by the public functions. */
 
 class BranchHistory {
  public:
@@ -21,23 +19,20 @@ class BranchHistory {
   }
   ~BranchHistory() {};
 
-  /** Returns the numBits most recent bits of the branch history.  Maximum
+  /** Returns the 'numBits' most recent bits of the branch history.  Maximum
    * number of bits returnable is 64 to allow it to be provided in a 64-bit
    * integer. */
   uint64_t getHistory(uint8_t numBits) {
-//    std::cout << "getHistory" << std::endl;
     assert(numBits <= 64 && "Cannot get more than 64 bits without rolling");
     assert(numBits <= size_ && "Cannot get more bits of branch history than "
            "the size of the history");
     return (history_[0] & ((1 << numBits) - 1));
   }
 
-  /** returns the global history folded over on itself to get a bitmap of the
-   * size specified by numBits.  The global history is folded by taking an
-   * XOR hash with the overflowing bits to get an output of 'length' bits.
-   * */
+  /** Returns 'numBits' of the global history folded over on itself to get a
+   * value of size 'length'.  The global history is folded by taking an
+   * XOR hash with the overflowing bits to get an output of 'length' bits. */
   uint64_t getFolded(uint8_t numBits, uint8_t length) {
-//    std::cout << "getFolded" << std::endl;
     assert(numBits <= size_ && "Cannot get more bits of branch history than "
            "the size of the history");
     uint64_t output = 0;
@@ -46,7 +41,6 @@ class BranchHistory {
     uint64_t endIndex = numBits - 1;
 
     while (startIndex <= numBits) {
-//      std::cout << "in while loop :(" << std::endl;
       output ^= ((history_[startIndex / 64] >> startIndex) &
                  ((1 << (numBits - startIndex)) - 1));
 
@@ -64,9 +58,8 @@ class BranchHistory {
     return output;
   }
 
-  /** Adds a branch outcome to the global history */
+  /** Adds a branch outcome ('isTaken') to the global history */
   void addHistory(bool isTaken) {
-//    std::cout << "addHistory" << std::endl;
     for (int8_t i = size_ / 64; i >= 0; i--) {
       history_[i] <<= 1;
       if (i == 0) {
@@ -78,12 +71,11 @@ class BranchHistory {
   }
 
   /** Updates the state of a branch that has already been added to the global
-   * history at 'position', where position is 0-indexed and starts from the
-   * least-significant bit.  I.e., to update the most recently added branch
-   * outcome, position would be 0.
+   * history at 'position', where 'position' is 0-indexed and starts from the
+   * most recent history.  I.e., to update the most recently added branch
+   * outcome, 'position' would be 0.
    * */
   void updateHistory(bool isTaken, uint64_t position) {
-//    std::cout << "updateHistory" << std::endl;
     if (position < size_) {
       uint8_t vectIndex = position / 64;
       uint8_t bitIndex = position % 64;
@@ -91,9 +83,8 @@ class BranchHistory {
     }
   }
 
-  /** removes the most recently added branch from the history */
+  /** Removes the most recently added branch from the history */
   void rollBack() {
-//    std::cout << "rollBack" << std::endl;
     for (uint8_t i = 0; i <= (size_ / 64); i++) {
       history_[i] >>= 1;
       if (i < (size_ / 64)) {
@@ -106,11 +97,11 @@ class BranchHistory {
   /** The number of bits of branch history stored in this branch history */
   uint64_t size_;
 
-  /** A vector containing this bits of the branch history.  The bits are
+  /** A vector containing the bits of the branch history.  The bits are
    * arranged such that the most recent branches are stored in uint64_t at
    * index 0 of the vector, then the next most recent at index 1 and so forth.
-   * Within each uint64_t, the most recent branches are recorded int he
-   * least-significant bits */
+   * Within each uint64_t, the most recent branches are recorded in the
+   * least-significant bits. */
   std::vector<uint64_t> history_;
 };
 
